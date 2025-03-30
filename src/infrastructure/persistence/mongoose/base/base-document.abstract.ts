be214ebc3fc8923b958c133b0ad107c0880c3621
imkzvd <imkzvd@gmail.com>
2025-03-31 02:51:07 +0300

    feat: added main mongoose module and base abstract documents
    Refs: #6

---
+++ src/infrastructure/persistence/mongoose/base/base-document.abstract.ts
@@ -0,0 +1,16 @@
+import { Prop } from '@nestjs/mongoose';
+import { Schema, Types } from 'mongoose';
+
+export abstract class BaseDocument {
+  @Prop({
+    required: true,
+    type: Schema.Types.ObjectId,
+  })
+  public readonly _id: Types.ObjectId;
+
+  @Prop({ required: false, type: Date, default: () => new Date() })
+  public readonly createdAt: Date;
+
+  @Prop({ required: false, type: Date, default: () => new Date() })
+  public readonly updatedAt: Date;
+}
