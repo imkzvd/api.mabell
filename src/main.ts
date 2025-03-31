c66aa204565cc15519802f7ed6556f8c96ac8498
imkzvd <imkzvd@gmail.com>
2025-03-31 03:08:32 +0300

    feat: rename admin app module
    Refs: #6

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
-import { AppModule } from './app.module';
+import { AdminAppModule } from './presenters/rest/admin/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const adminApp = await NestFactory.create(AdminAppModule);
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Mabell API')
+    .setTitle('Admin API')
     .setDescription('The API description')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, documentFactory);
+  const documentFactory = () => SwaggerModule.createDocument(adminApp, swaggerConfig);
+  SwaggerModule.setup('swagger', adminApp, documentFactory);
 
-  app.use(cookieParser());
+  adminApp.use(cookieParser());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await adminApp.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
