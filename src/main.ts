230e01f5d76d1df3fd04edf4b02ad7205436c59a
imkzvd <imkzvd@gmail.com>
2025-03-31 03:09:44 +0300

    feat: added global validation pipe in admin app module
    Closes: #6

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AdminAppModule } from './presenters/rest/admin/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const adminApp = await NestFactory.create(AdminAppModule);
@@ -15,6 +16,12 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', adminApp, documentFactory);
 
   adminApp.use(cookieParser());
+  adminApp.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      skipNullProperties: true,
+    }),
+  );
 
   await adminApp.listen(process.env.PORT ?? 3000);
 }
