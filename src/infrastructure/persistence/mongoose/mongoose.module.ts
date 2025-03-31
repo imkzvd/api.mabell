63cadd8ea8cbe212d93c56a8679ca708af265c9d
imkzvd <imkzvd@gmail.com>
2025-04-01 02:33:12 +0300

    feat: added mongo uri by env variables
    Refs: #18

--- src/infrastructure/persistence/mongoose/mongoose.module.ts
+++ src/infrastructure/persistence/mongoose/mongoose.module.ts
@@ -1,10 +1,16 @@
+import * as process from 'process';
 import { Global, Module } from '@nestjs/common';
 import { MongooseModule as MongooseNestModule } from '@nestjs/mongoose';
 import { AdminModule } from './modules/admin/admin.module';
 
 @Global()
 @Module({
-  imports: [MongooseNestModule.forRoot('###'), AdminModule],
+  imports: [
+    MongooseNestModule.forRoot(
+      `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_INITDB_HOST}`,
+    ),
+    AdminModule,
+  ],
   exports: [AdminModule],
 })
 export class MongooseModule {}
