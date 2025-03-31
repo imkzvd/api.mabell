404ecdd3f524968bb721fbc00b2b333fbb296f97
imkzvd <imkzvd@gmail.com>
2025-04-01 02:07:58 +0300

    feat: added global server static module for sharing tmp files
    Refs: #17

--- src/presenters/rest/admin/app.module.ts
+++ src/presenters/rest/admin/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 import { MongooseModule } from '../../../infrastructure/persistence/mongoose/mongoose.module';
 import { AdminsModule } from './controllers/admins/admins.module';
 import { UploadsModule } from './controllers/uploads/uploads.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'tmp'),
+      serveRoot: '/tmp',
+    }),
     ConfigModule.forRoot(),
     CqrsModule.forRoot(),
     MongooseModule,
