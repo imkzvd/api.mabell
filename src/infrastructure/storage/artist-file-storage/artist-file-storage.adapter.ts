b2959ced9ae163b9fda131defb92efa9a8d7773a
imkzvd <imkzvd@gmail.com>
2025-04-09 04:37:07 +0300

    feat: updated artist file storage
    Refs: #33

--- src/infrastructure/storage/artist-file-storage/artist-file-storage.adapter.ts
+++ src/infrastructure/storage/artist-file-storage/artist-file-storage.adapter.ts
@@ -1,75 +1,97 @@
-import * as path from 'path';
-import { TmpFileDTO } from '../../../core/app/components/upload/dtos/tmp-file.dto';
+import { Inject } from '@nestjs/common';
 import { StoredFileDTO } from '../../../core/app/common/dtos/file/stored-file.dto';
 import { FileStorage } from '../base/file-storage.abstract';
 import { ArtistFileStorage } from '../../../core/app/components/artist/ports/storage/artist-file-storage.port';
 import { ArtistId } from '../../../core/domain/components/artist/artist.entity';
+import {
+  TMP_FILE_STORAGE_DI_TOKEN,
+  TmpFileStorage,
+} from '../../../core/app/components/upload/storage/tmp-file-storage.port';
+import { BadRequestException } from '../../../core/shared/exceptions';
 
 export class ArtistFileStorageAdapter extends FileStorage implements ArtistFileStorage {
-  private readonly _avatarFileName: string = 'avatar.webp';
-  private readonly _coverFileName: string = 'cover.webp';
-
-  constructor() {
+  constructor(
+    @Inject(TMP_FILE_STORAGE_DI_TOKEN)
+    private readonly _tmpFileStorage: TmpFileStorage,
+  ) {
     super('artists');
   }
 
-  async saveAvatar(id: ArtistId, file: TmpFileDTO): Promise<StoredFileDTO> {
-    const isArtistDirExists = await this.existsDirectoryById(id);
+  async saveArtistAvatar(artistId: ArtistId, fileId: string): Promise<StoredFileDTO> {
+    const tmpFileData = await this._tmpFileStorage.findById(fileId);
 
-    if (!isArtistDirExists) {
-      await this.createDirectory(id);
+    if (!tmpFileData) {
+      throw new BadRequestException('The file has not been uploaded');
     }
 
-    const absoluteFilePath = this.getAbsolutePath(id, this._avatarFileName);
-    const relativeFilePath = this.getRelativePath(absoluteFilePath);
+    await this.createArtistDirectoryById(artistId);
+    const { fileName, absPath, relPath } = this.getArtistAvatarMetaById(artistId);
+    await this.convertAndSaveImage(tmpFileData.path, absPath);
+    await this._tmpFileStorage.deleteById(tmpFileData.id);
 
-    await this.convertImageToWebp(file.path, absoluteFilePath);
-
-    return new StoredFileDTO(
-      this._avatarFileName,
-      relativeFilePath,
-      absoluteFilePath,
-      file.size,
-      file.type,
-    );
+    return new StoredFileDTO(fileName, relPath, absPath, tmpFileData.size, tmpFileData.type);
   }
 
-  deleteAvatar(id: ArtistId): Promise<void> {
-    const artistDirPath = this.getDirectoryById(id);
-    const artistAvatarPath = path.join(artistDirPath, this._avatarFileName);
+  deleteArtistAvatar(id: ArtistId): Promise<void> {
+    const { absPath } = this.getArtistAvatarMetaById(id);
 
-    return this.deleteFileByIdFromDirectory(id, artistAvatarPath);
+    return this.deleteByPath(absPath);
   }
 
-  async saveCover(id: ArtistId, file: TmpFileDTO): Promise<StoredFileDTO> {
-    const isArtistDirExists = await this.existsDirectoryById(id);
+  async saveArtistCover(id: ArtistId, fileId: string): Promise<StoredFileDTO> {
+    const tmpFileData = await this._tmpFileStorage.findById(fileId);
 
-    if (!isArtistDirExists) {
-      await this.createDirectory(id);
+    if (!tmpFileData) {
+      throw new BadRequestException('The file has not been uploaded');
     }
 
-    const absoluteFilePath = this.getAbsolutePath(id, this._coverFileName);
-    const relativeFilePath = this.getRelativePath(absoluteFilePath);
+    await this.createArtistDirectoryById(id);
+    const { fileName, absPath, relPath } = this.getArtistCoverMetaById(id);
+    await this.convertAndSaveImage(tmpFileData.path, absPath);
+    await this._tmpFileStorage.deleteById(tmpFileData.id);
 
-    await this.convertImageToWebp(file.path, absoluteFilePath);
-
-    return new StoredFileDTO(
-      this._coverFileName,
-      relativeFilePath,
-      absoluteFilePath,
-      file.size,
-      file.type,
-    );
+    return new StoredFileDTO(fileName, relPath, absPath, tmpFileData.size, tmpFileData.type);
   }
 
-  deleteCover(id: ArtistId): Promise<void> {
-    const artistDirPath = this.getDirectoryById(id);
-    const artistCoverPath = path.join(artistDirPath, this._coverFileName);
+  deleteArtistCover(id: ArtistId): Promise<void> {
+    const { absPath } = this.getArtistCoverMetaById(id);
 
-    return this.deleteFileByIdFromDirectory(id, artistCoverPath);
+    return this.deleteByPath(absPath);
   }
 
-  async deleteDirectory(id: ArtistId): Promise<void> {
-    return this.deleteDirectoryById(id);
+  deleteArtistDirectory(id: ArtistId): Promise<void> {
+    const { absPath } = this.resolveProjectPath(id);
+
+    return this.deleteByPath(absPath);
+  }
+
+  private createArtistDirectoryById(id: ArtistId): Promise<{ absPath: string; relPath: string }> {
+    return this.createDirectory(id);
+  }
+
+  private getArtistAvatarMetaById(id: ArtistId): {
+    fileName: string;
+    absPath: string;
+    relPath: string;
+  } {
+    const fileName = 'avatar.webp';
+
+    return {
+      fileName,
+      ...this.resolveProjectPath(id, fileName),
+    };
+  }
+
+  private getArtistCoverMetaById(id: ArtistId): {
+    fileName: string;
+    absPath: string;
+    relPath: string;
+  } {
+    const fileName = 'cover.webp';
+
+    return {
+      fileName,
+      ...this.resolveProjectPath(id, fileName),
+    };
   }
 }
--- src/infrastructure/storage/artist-file-storage/artist-file-storage.module.ts
+++ src/infrastructure/storage/artist-file-storage/artist-file-storage.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArtistFileStorageAdapter } from './artist-file-storage.adapter';
 import { ARTIST_FILE_STORAGE_DI_TOKEN } from '../../../core/app/components/artist/ports/storage/artist-file-storage.port';
+import { TmpFileStorageModule } from '../tmp-fs-storage/tmp-file-storage.module';
 
 @Module({
+  imports: [TmpFileStorageModule],
   providers: [
     {
       provide: ARTIST_FILE_STORAGE_DI_TOKEN,
