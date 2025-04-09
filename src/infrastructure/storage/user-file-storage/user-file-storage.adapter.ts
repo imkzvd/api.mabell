8915b614dbb16f1e4ad5ce9f2467ca65b8d36818
imkzvd <imkzvd@gmail.com>
2025-04-09 04:32:39 +0300

    feat: updated user file storage module for new base file storage
    Refs: #33

--- src/infrastructure/storage/user-file-storage/user-file-storage.adapter.ts
+++ src/infrastructure/storage/user-file-storage/user-file-storage.adapter.ts
@@ -1,46 +1,63 @@
-import * as path from 'path';
+import { Inject } from '@nestjs/common';
 import { UserFileStorage } from '../../../core/app/components/user/ports/storage/user-file-storage.port';
-import { TmpFileDTO } from '../../../core/app/components/upload/dtos/tmp-file.dto';
 import { UserId } from '../../../core/domain/components/user/user.entity';
 import { StoredFileDTO } from '../../../core/app/common/dtos/file/stored-file.dto';
 import { FileStorage } from '../base/file-storage.abstract';
+import {
+  TMP_FILE_STORAGE_DI_TOKEN,
+  TmpFileStorage,
+} from '../../../core/app/components/upload/storage/tmp-file-storage.port';
+import { BadRequestException } from '../../../core/shared/exceptions';
 
 export class UserFileStorageAdapter extends FileStorage implements UserFileStorage {
-  private readonly _avatarFileName: string = 'avatar.webp';
-
-  constructor() {
+  constructor(
+    @Inject(TMP_FILE_STORAGE_DI_TOKEN)
+    private readonly _tmpFileStorage: TmpFileStorage,
+  ) {
     super('users');
   }
 
-  async saveAvatar(id: UserId, file: TmpFileDTO): Promise<StoredFileDTO> {
-    const isUserDirExists = await this.existsDirectoryById(id);
+  async saveUserAvatar(id: UserId, fileId: string): Promise<StoredFileDTO> {
+    const tmpFileData = await this._tmpFileStorage.findById(fileId);
 
-    if (!isUserDirExists) {
-      await this.createDirectory(id);
+    if (!tmpFileData) {
+      throw new BadRequestException('The file has not been uploaded');
     }
 
-    const absoluteFilePath = this.getAbsolutePath(id, this._avatarFileName);
-    const relativeFilePath = this.getRelativePath(absoluteFilePath);
+    const { fileName, absPath, relPath } = this.getUserAvatarMetaById(id);
+    await this.createUserDirectoryById(id);
+    await this.convertAndSaveImage(tmpFileData.path, absPath);
+    await this._tmpFileStorage.deleteById(tmpFileData.id);
 
-    await this.convertImageToWebp(file.path, absoluteFilePath);
-
-    return new StoredFileDTO(
-      this._avatarFileName,
-      relativeFilePath,
-      absoluteFilePath,
-      file.size,
-      file.type,
-    );
+    return new StoredFileDTO(fileName, relPath, absPath, tmpFileData.size, tmpFileData.type);
   }
 
-  deleteAvatar(id: UserId): Promise<void> {
-    const userDirPath = this.getDirectoryById(id);
-    const userAvatarPath = path.join(userDirPath, this._avatarFileName);
+  deleteUserAvatar(id: UserId): Promise<void> {
+    const { absPath } = this.getUserAvatarMetaById(id);
 
-    return this.deleteFileByIdFromDirectory(id, userAvatarPath);
+    return this.deleteByPath(absPath);
   }
 
-  async deleteDirectory(id: UserId): Promise<void> {
-    return this.deleteDirectoryById(id);
+  async deleteUserDirectory(id: UserId): Promise<void> {
+    const { absPath } = this.resolveProjectPath(id);
+
+    return this.deleteByPath(absPath);
+  }
+
+  private createUserDirectoryById(id: UserId): Promise<{ absPath: string; relPath: string }> {
+    return this.createDirectory(id);
+  }
+
+  private getUserAvatarMetaById(id: UserId): {
+    fileName: string;
+    absPath: string;
+    relPath: string;
+  } {
+    const fileName = 'avatar.webp';
+
+    return {
+      fileName,
+      ...this.resolveProjectPath(id, fileName),
+    };
   }
 }
--- src/infrastructure/storage/user-file-storage/user-file-storage.module.ts
+++ src/infrastructure/storage/user-file-storage/user-file-storage.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { USER_FILE_STORAGE_DI_TOKEN } from '../../../core/app/components/user/ports/storage/user-file-storage.port';
 import { UserFileStorageAdapter } from './user-file-storage.adapter';
+import { TmpFileStorageModule } from '../tmp-fs-storage/tmp-file-storage.module';
 
 @Module({
+  imports: [TmpFileStorageModule],
   providers: [
     {
       provide: USER_FILE_STORAGE_DI_TOKEN,
