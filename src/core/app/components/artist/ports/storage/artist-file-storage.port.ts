873b29d3ff303a30ccff080241861bd871729b24
imkzvd <imkzvd@gmail.com>
2025-04-14 20:46:38 +0300

    feat: updated artist file storage to save track files
    Refs: #38

--- src/core/app/components/artist/ports/storage/artist-file-storage.port.ts
+++ src/core/app/components/artist/ports/storage/artist-file-storage.port.ts
@@ -1,6 +1,7 @@
 import { StoredFileDTO } from '../../../../common/dtos/file/stored-file.dto';
 import { ArtistId } from '../../../../../domain/components/artist/artist.entity';
 import { AlbumId } from '../../../../../domain/components/album/album.entity';
+import { TrackId } from '../../../../../domain/components/track/track.entity';
 
 export const ARTIST_FILE_STORAGE_DI_TOKEN = Symbol('ARTIST_FILE_STORAGE_DI_TOKEN');
 
@@ -13,4 +14,11 @@ export interface ArtistFileStorage {
   saveAlbumCover(artistId: ArtistId, albumId: AlbumId, fileId: string): Promise<StoredFileDTO>;
   deleteAlbumCover(artistId: ArtistId, albumId: AlbumId): Promise<void>;
   deleteAlbumDirectory(artistId: ArtistId, albumId: AlbumId): Promise<void>;
+  saveTrack(
+    artistId: ArtistId,
+    albumId: AlbumId,
+    trackId: TrackId,
+    fileId: string,
+  ): Promise<StoredFileDTO>;
+  deleteTrack(artistId: ArtistId, albumId: AlbumId, trackId: TrackId): Promise<void>;
 }
