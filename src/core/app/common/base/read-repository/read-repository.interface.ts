bebd34771ef91681aa08547f59a341df91d645f2
imkzvd <imkzvd@gmail.com>
2025-04-17 03:16:20 +0300

    feat: updated app common read repository interface
    Refs: #42

--- src/core/app/common/base/read-repository/read-repository.interface.ts
+++ src/core/app/common/base/read-repository/read-repository.interface.ts
@@ -2,18 +2,28 @@ import { OffsetLimitPaginationDTO } from '../../dtos/offset-limit-pagination/off
 import { OffsetLimitPaginationResponseDTO } from '../../dtos/offset-limit-pagination/offset-limit-pagination-response.dto';
 import { QueryFilter } from '../../../../domain/common/repository/query-filter.abstract';
 
-export interface ReadRepository<DTO, SimplifiedDTO, Filter extends QueryFilter> {
-  findById(id: string): Promise<DTO | null>;
-  findByIds(ids: string[]): Promise<{
-    items: DTO[];
+export interface ReadRepository<
+  DTO extends Record<string, any>,
+  Filter extends QueryFilter = QueryFilter,
+  WithRelationsDTO extends Record<string, any> = DTO,
+> {
+  findById<WithRelations extends boolean = false>(
+    id: string,
+    withRelations?: WithRelations,
+  ): Promise<(WithRelations extends true ? WithRelationsDTO : DTO) | null>;
+  findByIds<WithRelations extends boolean = false>(
+    ids: string[],
+    withRelations?: WithRelations,
+  ): Promise<{
+    items: (WithRelations extends true ? WithRelationsDTO : DTO)[];
     foundIds: string[];
     missingIds: string[];
   }>;
-  find(
+  find<WithRelations extends boolean = false>(
     options?: Partial<{
       filter: Filter;
+      withRelations: WithRelations;
       pagination: OffsetLimitPaginationDTO;
-      projection: Partial<Record<keyof SimplifiedDTO, 0 | 1>>;
     }>,
-  ): Promise<OffsetLimitPaginationResponseDTO<SimplifiedDTO>>;
+  ): Promise<OffsetLimitPaginationResponseDTO<WithRelations extends true ? WithRelationsDTO : DTO>>;
 }
