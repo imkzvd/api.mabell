dd90ea3f73ab71c92c7d2c34f4d9e67575bfac4a
imkzvd <imkzvd@gmail.com>
2025-04-21 15:24:38 +0300

    feat: updated app artist repository port and artist dtos
    Refs: #42

--- src/core/app/components/artist/ports/repository/artist-read-repository.port.ts
+++ src/core/app/components/artist/ports/repository/artist-read-repository.port.ts
@@ -1,6 +1,6 @@
 import { ArtistFilter } from './artist.filter';
 import { ReadRepository } from '../../../../common/base/read-repository/read-repository.interface';
-import { ArtistDTO } from '../../dtos/artist.dto';
+import { ArtistDTO } from './dtos/artist.dto';
 
 export const ARTIST_READ_REPOSITORY_DI_TOKEN = Symbol('ARTIST_READ_REPOSITORY_DI_TOKEN');
 
