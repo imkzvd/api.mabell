c283d9b37729a7a651e9519895e5b5e975172f6b
imkzvd <imkzvd@gmail.com>
2025-04-25 03:28:28 +0300

    feat: added domain playlist component
    Refs: #50

---
+++ src/core/domain/components/playlist/playlist.factory.ts
@@ -0,0 +1,37 @@
+import { Playlist, PlaylistId } from './playlist.entity';
+import { UserId } from '../user/user.entity';
+import { TrackId } from '../track/track.entity';
+import { NameVO } from './vos/name.vo';
+import { GenreVO } from '../../common/vos/genre.vo';
+import { HexColorVO } from '../../common/vos/hex-color.vo';
+import { DescriptionVO } from './vos/description.vo';
+
+export class PlaylistFactory {
+  static create(props: {
+    id: PlaylistId;
+    owner: UserId;
+    name: string;
+    genres?: string[];
+    cover?: string | null;
+    color?: string | null;
+    description?: string;
+    tracks?: { id: string; addedAt: Date }[];
+    isPublic?: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }) {
+    return new Playlist(
+      props.id,
+      props.owner,
+      NameVO.create(props.name),
+      props.genres?.map((genre: string) => GenreVO.create(genre)) || [],
+      props.cover || null,
+      props.color ? HexColorVO.create(props.color) : null,
+      DescriptionVO.create(props.description || ''),
+      new Map(props.tracks?.map(({ id, addedAt }) => [id as TrackId, addedAt])),
+      props.isPublic ?? false,
+      props.createdAt || new Date(),
+      props.updatedAt || new Date(),
+    );
+  }
+}
