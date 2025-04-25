0671d5d11b57206b054dda595f1c361c04dd9e5a
imkzvd <imkzvd@gmail.com>
2025-04-25 15:22:44 +0300

    feat: updated main start file to run admin and client apps together
    Closes: #49

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,56 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AdminAppModule } from './presenters/rest/admin/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ClientAppModule } from './presenters/rest/client/app.module';
 
-async function bootstrap() {
-  const adminApp = await NestFactory.create(AdminAppModule);
+async function runAdminApp() {
+  const app = await NestFactory.create(AdminAppModule);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Admin API')
     .setDescription('The API description')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(adminApp, swaggerConfig);
-  SwaggerModule.setup('swagger', adminApp, documentFactory);
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
-  adminApp.use(cookieParser());
-  adminApp.useGlobalPipes(
+  app.use(cookieParser());
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       skipNullProperties: true,
     }),
   );
 
-  await adminApp.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.ADMIN_APP_PORT ?? 3000);
 }
 
-bootstrap();
+async function runClientApp() {
+  const app = await NestFactory.create(ClientAppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Client API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      skipNullProperties: true,
+    }),
+  );
+
+  await app.listen(process.env.CLIENT_APP_PORT ?? 4000);
+}
+
+void runAdminApp().then(() => {
+  console.log('Admin API Server started');
+});
+
+void runClientApp().then(() => {
+  console.log('Client API Server started');
+});
