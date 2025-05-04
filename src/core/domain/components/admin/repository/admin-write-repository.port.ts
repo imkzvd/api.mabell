5e3d602916957a7097f3136d18bbf64352cdab87
imkzvd <imkzvd@gmail.com>
2025-05-04 17:39:17 +0300

    feat: updated admin write repository interface
    Refs: #57

--- src/core/domain/components/admin/repository/admin-write-repository.port.ts
+++ src/core/domain/components/admin/repository/admin-write-repository.port.ts
@@ -1,7 +1,11 @@
-import { WriteRepository } from '../../../common/repository/write-repository.interface';
-import { Admin, AdminId } from '../admin.entity';
-import { AdminFilter } from './admin.filter';
+import { Admin } from '../admin.entity';
 
 export const ADMIN_WRITE_REPOSITORY_DI_TOKEN = Symbol('ADMIN_WRITE_REPOSITORY_DI_TOKEN');
 
-export type AdminWriteRepository = WriteRepository<Admin, AdminId, AdminFilter>;
+export interface AdminWriteRepository {
+  save(entity: Admin): Promise<void>;
+  deleteById(id: string): Promise<boolean>;
+  findById(id: string): Promise<Admin | null>;
+  existsByUsername(username: string): Promise<boolean>;
+  getNextIndex(): Promise<number>;
+}
