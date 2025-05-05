04fb0f4a9f4e80cda2f9542d418f25bb7eb80dee
imkzvd <imkzvd@gmail.com>
2025-05-05 15:27:55 +0300

    feat: updated "get-admin" query
    Refs: #57

--- src/core/app/components/admin/queries/get-admin/get-admin.handler.ts
+++ src/core/app/components/admin/queries/get-admin/get-admin.handler.ts
@@ -1,20 +1,20 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
-import { GetAdminByIdQuery } from './get-admin-by-id.query';
+import { GetAdminQuery } from './get-admin.query';
 import {
   ADMIN_READ_REPOSITORY_DI_TOKEN,
   AdminReadRepository,
 } from '../../repository/admin-read-repository.port';
 import AdminMapper from '../dtos/admin.mapper';
 
-@QueryHandler(GetAdminByIdQuery)
-export class GetAdminByIdHandler implements IQueryHandler<GetAdminByIdQuery> {
+@QueryHandler(GetAdminQuery)
+export class GetAdminHandler implements IQueryHandler<GetAdminQuery> {
   constructor(
     @Inject(ADMIN_READ_REPOSITORY_DI_TOKEN)
     private readonly _adminReadRepository: AdminReadRepository,
   ) {}
 
-  async execute({ id }: GetAdminByIdQuery) {
+  async execute({ id }: GetAdminQuery) {
     const foundAdmin = await this._adminReadRepository.findById(id);
 
     return foundAdmin ? AdminMapper.toDTO(foundAdmin) : null;
--- src/core/app/components/admin/queries/get-admin/get-admin.query.ts
+++ src/core/app/components/admin/queries/get-admin/get-admin.query.ts
@@ -1,7 +1,7 @@
 import { Query } from '@nestjs/cqrs';
 import { AdminDTO } from '../dtos/admin.dto';
 
-export class GetAdminByIdQuery extends Query<AdminDTO | null> {
+export class GetAdminQuery extends Query<AdminDTO | null> {
   constructor(public readonly id: string) {
     super();
   }
