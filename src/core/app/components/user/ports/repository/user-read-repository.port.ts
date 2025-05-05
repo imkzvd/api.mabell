e468fed95fd2dc45eae67940adc72766403d98f3
imkzvd <imkzvd@gmail.com>
2025-05-05 15:48:47 +0300

    feat: updated user read repository port
    Refs: #57

--- src/core/app/components/user/ports/repository/user-read-repository.port.ts
+++ src/core/app/components/user/ports/repository/user-read-repository.port.ts
@@ -1,7 +1,12 @@
-import { UserFilter } from './user.filter';
-import { ReadRepository } from '../../../../common/base/read-repository/read-repository.interface';
 import { UserDTO } from './dtos/user.dto';
 
 export const USER_READ_REPOSITORY_DI_TOKEN = Symbol('USER_READ_REPOSITORY_DI_TOKEN');
 
-export type UserReadRepository = ReadRepository<UserDTO, UserFilter>;
+export interface UserReadRepository {
+  findById(
+    id: string,
+    options?: Partial<{
+      isPublic: boolean;
+    }>,
+  ): Promise<UserDTO | null>;
+}
