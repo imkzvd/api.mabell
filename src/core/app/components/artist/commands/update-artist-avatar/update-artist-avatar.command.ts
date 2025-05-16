d17e8fcb10f481afd989fc872b15e320155fbfb0
imkzvd <imkzvd@gmail.com>
2025-05-16 17:39:40 +0300

    feat: updated "update-album-cover", "update-artist-avatar", "update-artist-cover" commands:
    * made "fileId" payload parameter optional
    
    Refs: #66

--- src/core/app/components/artist/commands/update-artist-avatar/update-artist-avatar.command.ts
+++ src/core/app/components/artist/commands/update-artist-avatar/update-artist-avatar.command.ts
@@ -3,10 +3,10 @@ import { Command } from '@nestjs/cqrs';
 export class UpdateArtistAvatarCommand extends Command<void> {
   constructor(
     public readonly id: string,
-    public readonly payload: {
-      fileId: string;
-      color?: string | null;
-    },
+    public readonly payload: Partial<{
+      fileId: string | null;
+      color: string | null;
+    }>,
   ) {
     super();
   }
