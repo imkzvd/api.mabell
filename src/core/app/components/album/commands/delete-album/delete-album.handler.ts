de115aabda70da75ac898330bc7e51f286f59243
imkzvd <imkzvd@gmail.com>
2025-05-16 17:53:15 +0300

    fix: update delete album command handler
    Closes: #67

--- src/core/app/components/album/commands/delete-album/delete-album.handler.ts
+++ src/core/app/components/album/commands/delete-album/delete-album.handler.ts
@@ -10,12 +10,18 @@ import {
   ARTIST_FILE_STORAGE_DI_TOKEN,
   ArtistFileStorage,
 } from '../../../artist/ports/storage/artist-file-storage.port';
+import {
+  TRACK_WRITE_REPOSITORY_DI_TOKEN,
+  TrackWriteRepository,
+} from '../../../../../domain/components/track/repository/track-write-repository.port';
 
 @CommandHandler(DeleteAlbumCommand)
 export class DeleteAlbumHandler implements ICommandHandler<DeleteAlbumCommand> {
   constructor(
     @Inject(ALBUM_WRITE_REPOSITORY_DI_TOKEN)
     private readonly _albumWriteRepository: AlbumWriteRepository,
+    @Inject(TRACK_WRITE_REPOSITORY_DI_TOKEN)
+    private readonly _trackWriteRepository: TrackWriteRepository,
     @Inject(ARTIST_FILE_STORAGE_DI_TOKEN)
     private readonly _artistFileStorage: ArtistFileStorage,
   ) {}
@@ -28,6 +34,7 @@ export class DeleteAlbumHandler implements ICommandHandler<DeleteAlbumCommand> {
     }
 
     await this._albumWriteRepository.deleteById(id);
+    await this._trackWriteRepository.deleteByAlbumId(id);
 
     return this._artistFileStorage.deleteAlbumDirectory(
       foundAlbum.getMainArtist(),
