0b0fa001f7c22f3636cf78710a2a46dc6184215c
imkzvd <imkzvd@gmail.com>
2025-06-02 19:46:16 +0300

    feat: renamed track event subscriber file
    Refs: #73

--- src/presenters/rest/admin/controllers/tracks/tracks.module.ts
+++ src/presenters/rest/admin/controllers/tracks/tracks.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TracksController } from './tracks.controller';
 import { TrackService } from '../../../../../core/app/components/track/track.service';
-import { TrackSubscriber } from '../../../../../core/app/components/track/events/track.subscriber';
 import { AlbumService } from '../../../../../core/app/components/album/album.service';
 import { ArtistService } from '../../../../../core/app/components/artist/artist.service';
 import { CreateTrackHandler } from '../../../../../core/app/cqrs/track/commands/create-track/create-track.handler';
@@ -11,13 +10,13 @@ import { UpdateTrackHandler } from '../../../../../core/app/cqrs/track/commands/
 import { UpdateTrackFeatArtistsHandler } from '../../../../../core/app/cqrs/track/commands/update-track-feat-artists/update-track-feat-artists.handler';
 import { UpdateTrackFileHandler } from '../../../../../core/app/cqrs/track/commands/update-track-file/update-track-file.handler';
 import { GetTrackHandler } from '../../../../../core/app/cqrs/track/queries/get-track/get-track.handler';
+import { TrackEventSubscriber } from '../../../../../core/app/components/track/track.event-subscriber';
 
 @Module({
   providers: [
     ArtistService,
     AlbumService,
     TrackService,
-    TrackSubscriber,
     CreateTrackHandler,
     DeleteTrackHandler,
     DeleteTrackFileHandler,
@@ -25,6 +24,7 @@ import { GetTrackHandler } from '../../../../../core/app/cqrs/track/queries/get-
     UpdateTrackFeatArtistsHandler,
     UpdateTrackFileHandler,
     GetTrackHandler,
+    TrackEventSubscriber,
   ],
   controllers: [TracksController],
 })
