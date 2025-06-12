e3276b794cff79db620f8be475e288a8e1878dd0
imkzvd <imkzvd@gmail.com>
2025-06-12 17:21:14 +0300

    feat: updated typesense album module:
    * added factory
    * updated doc structure
    * updated collection schema
    
    Refs: #79

--- src/infrastructure/persistence/typesense/modules/album/album.collection.ts
+++ src/infrastructure/persistence/typesense/modules/album/album.collection.ts
@@ -1,6 +1,6 @@
 import { CollectionCreateSchema } from 'typesense/lib/Typesense/Collections';
 import { TypeSenseClient } from '../../client';
-import { AlbumDocument } from './album.document';
+import { Album } from './album.document';
 import { BaseCollection } from '../../base/base-collection.interface';
 import { IndexedAlbumDTO } from '../../../../../core/app/components/search/ports/search-service/dtos/indexed-album.dto';
 import AlbumMapper from './album.mapper';
@@ -13,11 +13,7 @@ export class AlbumCollection implements BaseCollection<IndexedAlbumDTO, AlbumWit
     enable_nested_fields: true,
     fields: [
       { name: 'name', type: 'string' },
-      { name: 'artists', type: 'object[]' },
       { name: 'artistNames', type: 'string[]' },
-      { name: 'isPublic', type: 'bool' },
-      { name: 'cover', type: 'string', optional: true },
-      { name: 'releaseAt', type: 'string', optional: true },
     ],
   };
 
@@ -28,13 +24,11 @@ export class AlbumCollection implements BaseCollection<IndexedAlbumDTO, AlbumWit
   async save(dto: AlbumWithArtistsDTO): Promise<void> {
     const mappedDoc = AlbumMapper.toDocument(dto);
 
-    await TypeSenseClient.collections<AlbumDocument>(this._collectionName)
-      .documents()
-      .upsert(mappedDoc);
+    await TypeSenseClient.collections<Album>(this._collectionName).documents().upsert(mappedDoc);
   }
 
   async searchByQuery(q: string): Promise<IndexedAlbumDTO[]> {
-    const result = await TypeSenseClient.collections<AlbumDocument>(this._collectionName)
+    const result = await TypeSenseClient.collections<Album>(this._collectionName)
       .documents()
       .search({ q, query_by: 'name,artistNames' });
 
@@ -42,15 +36,13 @@ export class AlbumCollection implements BaseCollection<IndexedAlbumDTO, AlbumWit
   }
 
   async deleteById(id: string): Promise<void> {
-    await TypeSenseClient.collections<AlbumDocument>(this._collectionName).documents(id).delete();
+    await TypeSenseClient.collections<Album>(this._collectionName).documents(id).delete();
   }
 
   async deleteByIds(ids: string[]): Promise<void> {
     const promiseQueue = Promise.all(
       ids.map((id) => {
-        return TypeSenseClient.collections<AlbumDocument>(this._collectionName)
-          .documents(id)
-          .delete();
+        return TypeSenseClient.collections<Album>(this._collectionName).documents(id).delete();
       }),
     );
 
@@ -62,7 +54,6 @@ export class AlbumCollection implements BaseCollection<IndexedAlbumDTO, AlbumWit
 
     if (isExistCollection) return;
 
-    // await TypeSenseClient.collections(this._collectionName).delete();
     await TypeSenseClient.collections().create(this._collectionSchema);
   }
 }
--- src/infrastructure/persistence/typesense/modules/album/album.mapper.ts
+++ src/infrastructure/persistence/typesense/modules/album/album.mapper.ts
@@ -1,30 +1,21 @@
-import { AlbumDocument } from './album.document';
+import { Album } from './album.document';
 import { IndexedAlbumDTO } from '../../../../../core/app/components/search/ports/search-service/dtos/indexed-album.dto';
-import { AlbumWithArtistsDTO } from '../../../../../core/domain/components/album/repository/dtos/album-with-artists.dto';
+import { AlbumFactory } from './album.factory';
+import { AlbumDTO } from '../../../../../core/app/components/album/dtos/album.dto';
+import { IndexedSimplifiedArtistDTO } from '../../../../../core/app/components/search/ports/search-service/dtos/indexed-simplified-artist.dto';
 
 class AlbumMapper {
-  toDocument(dto: AlbumWithArtistsDTO): AlbumDocument {
-    return new AlbumDocument(
-      dto.id,
-      dto.name,
-      dto.artists.map(({ id, name }) => ({ id, name })),
-      dto.artists.map(({ name }) => name),
-      dto.type,
-      dto.isPublic,
-      dto.cover || undefined,
-      dto.releaseAt || undefined,
-    );
+  toDocument(dto: AlbumDTO): Album {
+    return AlbumFactory.create(dto);
   }
 
-  toDTO(doc: AlbumDocument): IndexedAlbumDTO {
+  toDTO(doc: Album): IndexedAlbumDTO {
     return new IndexedAlbumDTO(
       doc.id,
       doc.name,
-      doc.artists,
+      doc.artists.map(({ id, name }) => new IndexedSimplifiedArtistDTO(id, name)),
       doc.type,
       doc.cover || null,
-      doc.releaseAt || null,
-      doc.isPublic,
     );
   }
 }
