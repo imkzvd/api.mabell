b8fdd4765af6c69a15888c5b42f78ecf817eb9b5
imkzvd <imkzvd@gmail.com>
2025-06-12 17:19:51 +0300

    feat: updated typesense artist module:
    * added factory
    * updated doc structure
    * updated collection schema
    
    Refs: #79

--- src/infrastructure/persistence/typesense/modules/artist/artist.collection.ts
+++ src/infrastructure/persistence/typesense/modules/artist/artist.collection.ts
@@ -1,7 +1,7 @@
 import { CollectionCreateSchema } from 'typesense/lib/Typesense/Collections';
 import { TypeSenseClient } from '../../client';
 import ArtistMapper from './artist.mapper';
-import { ArtistDocument } from './artist.document';
+import { Artist } from './artist.document';
 import { IndexedArtistDTO } from '../../../../../core/app/components/search/ports/search-service/dtos/indexed-artist.dto';
 import { BaseCollection } from '../../base/base-collection.interface';
 import { ArtistDTO } from '../../../../../core/domain/components/artist/repository/dtos/artist.dto';
@@ -12,9 +12,7 @@ export class ArtistCollection implements BaseCollection<IndexedArtistDTO, Artist
     name: this._collectionName,
     fields: [
       { name: 'name', type: 'string' },
-      { name: 'isPublic', type: 'bool' },
-      { name: 'birthName', type: 'string', optional: true },
-      { name: 'avatar', type: 'string', optional: true },
+      { name: 'isGlobal', type: 'bool' },
     ],
   };
 
@@ -25,21 +23,19 @@ export class ArtistCollection implements BaseCollection<IndexedArtistDTO, Artist
   async save(dto: ArtistDTO): Promise<void> {
     const mappedDoc = ArtistMapper.toDocument(dto);
 
-    await TypeSenseClient.collections<ArtistDocument>(this._collectionName)
-      .documents()
-      .upsert(mappedDoc);
+    await TypeSenseClient.collections<Artist>(this._collectionName).documents().upsert(mappedDoc);
   }
 
   async searchByQuery(q: string): Promise<IndexedArtistDTO[]> {
-    const result = await TypeSenseClient.collections<ArtistDocument>(this._collectionName)
+    const result = await TypeSenseClient.collections<Artist>(this._collectionName)
       .documents()
-      .search({ q, query_by: 'name,birthName' });
+      .search({ q, query_by: 'name' });
 
     return result.hits?.map(({ document }) => ArtistMapper.toDTO(document)) || [];
   }
 
   async deleteById(id: string): Promise<void> {
-    await TypeSenseClient.collections<ArtistDocument>(this._collectionName).documents(id).delete();
+    await TypeSenseClient.collections<Artist>(this._collectionName).documents(id).delete();
   }
 
   private async createCollection(): Promise<void> {
@@ -47,7 +43,6 @@ export class ArtistCollection implements BaseCollection<IndexedArtistDTO, Artist
 
     if (isExistCollection) return;
 
-    // await TypeSenseClient.collections(this._collectionName).delete();
     await TypeSenseClient.collections().create(this._collectionSchema);
   }
 }
