69837d46afb5dde20f415a8808053b67f013cc0b
imkzvd <imkzvd@gmail.com>
2025-06-16 20:31:29 +0300

    fix: deleted map transformation in cqrs playlist and user handlers
    Closes: #95

--- src/core/app/cqrs/user/queries/get-user/get-user.handler.ts
+++ src/core/app/cqrs/user/queries/get-user/get-user.handler.ts
@@ -2,15 +2,12 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { GetUserQuery } from './get-user.query';
 import { UserService } from '../../../../components/user/user.service';
-import UserMapper from '../../../../components/user/dtos/user.mapper';
 
 @QueryHandler(GetUserQuery)
 export class GetUserHandler implements IQueryHandler<GetUserQuery> {
   constructor(@Inject(UserService) private readonly _userService: UserService) {}
 
   async execute({ id, options }: GetUserQuery) {
-    const foundUser = await this._userService.getUser(id, options);
-
-    return foundUser ? UserMapper.toDTO(foundUser) : null;
+    return await this._userService.getUser(id, options);
   }
 }
