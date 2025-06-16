16ab2227c379e41f13b4708b4f017be35855d88b
imkzvd <imkzvd@gmail.com>
2025-06-16 21:50:11 +0300

    feat: added access token in response cookie in client login
    Refs: #97

--- src/presenters/rest/client/controllers/login/login.controller.ts
+++ src/presenters/rest/client/controllers/login/login.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Res, Req, HttpCode, HttpStatus } from '@nestjs/common';
 import { ApiBody, ApiNoContentResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import * as process from 'process';
 import { CommandBus } from '@nestjs/cqrs';
 import { LoginUserDTO } from './dtos/login-user.dto';
 import { LoginUserCommand } from '../../../../../core/app/cqrs/user/commands/login-user/login-user.command';
+import { CreateUserAccessTokenCommand } from '../../../../../core/app/cqrs/token/commands/create-user-access-token/create-user-access-token.command';
 
 @ApiTags('Login')
 @Controller('/login')
@@ -14,7 +17,23 @@ export class LoginController {
   @ApiNoContentResponse({ description: 'Success login', schema: { format: 'json' } })
   @HttpCode(HttpStatus.NO_CONTENT)
   @Post('/')
-  async login(@Body() dto: LoginUserDTO): Promise<void> {
-    await this._commandBus.execute(new LoginUserCommand(dto));
+  async login(
+    @Req() req: Request,
+    @Body() dto: LoginUserDTO,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<void> {
+    const loggedUserId = await this._commandBus.execute(new LoginUserCommand(dto));
+    const accessToken = await this._commandBus.execute(
+      new CreateUserAccessTokenCommand(loggedUserId),
+    );
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      sameSite: true,
+      maxAge:
+        (process.env.ACCESS_TOKEN_EXPIRATION
+          ? parseInt(process.env.ACCESS_TOKEN_EXPIRATION)
+          : 300) * 1000,
+    });
   }
 }
