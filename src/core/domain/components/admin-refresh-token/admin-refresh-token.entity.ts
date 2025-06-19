2b51b543d55ac3c3f7838dd8a07fb657579ac320
imkzvd <imkzvd@gmail.com>
2025-06-19 18:23:00 +0300

    feat: renamed domain "refresh-token" component to "admin-refresh-token" component
    Refs: #98

--- src/core/domain/components/admin-refresh-token/admin-refresh-token.entity.ts
+++ src/core/domain/components/admin-refresh-token/admin-refresh-token.entity.ts
@@ -1,22 +1,27 @@
-import { RefreshTokenId } from './types';
+import { AdminRefreshTokenId } from './types';
 import { AdminId } from '../admin/types';
-import { UserId } from '../user/types';
+import { AdminRole } from '../admin/constants/admin-roles';
 
-export class RefreshToken {
+export class AdminRefreshToken {
   constructor(
-    private readonly _id: RefreshTokenId,
-    private readonly _owner: AdminId | UserId,
+    private readonly _id: AdminRefreshTokenId,
+    private readonly _owner: AdminId,
+    private readonly _role: AdminRole,
     private readonly _ip: string,
     private readonly _userAgent: string,
     private readonly _createdAt: Date,
   ) {}
 
-  getId(): RefreshTokenId {
+  getId(): AdminRefreshTokenId {
     return this._id;
   }
 
-  getOwner(): UserId | AdminId {
+  getOwner(): AdminId {
     return this._owner;
+  }
+
+  getRole(): AdminRole {
+    return this._role;
   }
 
   getIp(): string {
