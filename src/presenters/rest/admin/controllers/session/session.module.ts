d4faa142f8cec201a502c1788020f73803b56e28
imkzvd <imkzvd@gmail.com>
2025-06-21 01:45:10 +0300

    fix: delete "ValidateRefreshTokenHandler" provider in admin session module
    Refs: #98

--- src/presenters/rest/admin/controllers/session/session.module.ts
+++ src/presenters/rest/admin/controllers/session/session.module.ts
@@ -3,11 +3,10 @@ import { SessionController } from './session.controller';
 import { DeleteAdminRefreshTokenHandler } from '../../../../../core/app/cqrs/token/commands/delete-admin-refresh-token/delete-admin-refresh-token.handler';
 import { AdminTokenService } from '../../../../../core/app/components/admin-token/admin-token.service';
 import { JWTModule } from '../../../../../infrastructure/security/jwt/jwt.module';
-import { ValidateRefreshTokenHandler } from '../../../../../core/app/cqrs/token/queries/validate-refresh-token/validate-refresh-token.handler';
 
 @Module({
   imports: [JWTModule],
-  providers: [AdminTokenService, DeleteAdminRefreshTokenHandler, ValidateRefreshTokenHandler],
+  providers: [AdminTokenService, DeleteAdminRefreshTokenHandler],
   controllers: [SessionController],
 })
 export class SessionModule {}
