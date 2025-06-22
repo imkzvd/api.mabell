89392f285f9c35de72602e7db52ab7f6f24b8ff1
imkzvd <imkzvd@gmail.com>
2025-06-22 21:21:08 +0300

    feat: enabled admin configs
    Refs: #107

--- src/presenters/rest/client/app.module.ts
+++ src/presenters/rest/client/app.module.ts
@@ -10,9 +10,20 @@ import { IdModule } from '../../../infrastructure/services/id/id.module';
 import { StorageModule } from '../../../infrastructure/storage/storage.module';
 import { InMemoryEventBusModule } from '../../../infrastructure/event-bus/in-memory-event-bus/in-memory-event-bus.module';
 import { LoginModule } from './controllers/login/login.module';
+import appConfig from '../admin/config/app.config';
+import corsConfig from '../admin/config/cors.config';
+import databaseConfig from '../admin/config/database.config';
+import jwtConfig from '../admin/config/jwt.config';
+import mailConfig from '../admin/config/mail.config';
+import redisConfig from '../admin/config/redis.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [appConfig, corsConfig, databaseConfig, jwtConfig, mailConfig, redisConfig],
+    }),
     ServeStaticModule.forRoot({
       rootPath: path.join(process.cwd(), 'tmp'),
       serveRoot: '/tmp',
