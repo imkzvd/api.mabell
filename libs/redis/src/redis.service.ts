d7ff838ccd22388905e3e4c477bb82ad6ff8fcea
imkzvd <imkzvd@gmail.com>
2025-06-23 04:19:20 +0300

    feat: updated "redis" infra lib
    Refs: #107

---
+++ libs/redis/src/redis.service.ts
@@ -0,0 +1,42 @@
+import { Redis } from 'ioredis';
+import * as process from 'process';
+import { Injectable } from '@nestjs/common';
+import { CacheService } from '../../../src/core/app/common/ports/cache.service.port';
+
+@Injectable()
+export class RedisService implements CacheService {
+  private readonly _client: Redis;
+
+  constructor() {
+    this._client = new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: parseInt(process.env.REDIS_PORT || '6379', 10),
+      password: process.env.REDIS_PASSWORD,
+    });
+  }
+
+  async get<T>(key: string): Promise<T | null> {
+    return (await this._client.get(key)) as T | null;
+  }
+
+  async set<T extends string | number = string>(
+    key: string,
+    value: T,
+    ttlSeconds?: number,
+  ): Promise<void> {
+    await this._client.set(
+      key,
+      value,
+      'EX',
+      ttlSeconds || parseInt(process.env.DEFAULT_TTL_SECONDS || '300', 10),
+    );
+  }
+
+  async del(key: string): Promise<void> {
+    await this._client.del(key);
+  }
+
+  async has(key: string): Promise<boolean> {
+    return !!(await this._client.exists(key));
+  }
+}
