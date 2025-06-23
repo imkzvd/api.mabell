8408a428ffb80339ef39a4fc75bd84a2d2288a0d
imkzvd <imkzvd@gmail.com>
2025-06-23 18:54:58 +0300

    feat: updated app "login" component in "core" lib
    Refs: #107

--- libs/core/src/app/components/login/login.service.ts
+++ libs/core/src/app/components/login/login.service.ts
@@ -1,26 +1,16 @@
-import { Inject } from '@nestjs/common';
-import { AdminId } from '../../../domain/components/admin/types';
-import { UserId } from '../../../domain/components/user/types';
+import { UnauthorizedException } from '@core/shared/exceptions';
+import { AdminId } from '@core/domain/components/admin/types';
+import { UserId } from '@core/domain/components/user/types';
+import { AdminReadRepository } from '@core/domain/components/admin/repository/admin-read-repository.port';
+import { UserReadRepository } from '@core/domain/components/user/repository/user-read-repository.port';
 import { LoginPayload } from './types';
-import {
-  ADMIN_READ_REPOSITORY_DI_TOKEN,
-  AdminReadRepository,
-} from '../../../domain/components/admin/repository/admin-read-repository.port';
-import {
-  USER_READ_REPOSITORY_DI_TOKEN,
-  UserReadRepository,
-} from '../../../domain/components/user/repository/user-read-repository.port';
-import { UnauthorizedException } from '../../../shared/exceptions';
-import {
-  PASSWORD_SERVICE_DI_TOKEN,
-  PasswordService,
-} from '../../common/ports/password-service.port';
+import { PasswordService } from '../../common/ports/password-service.port';
 
 export class LoginService {
   constructor(
-    @Inject(ADMIN_READ_REPOSITORY_DI_TOKEN) private readonly _adminRR: AdminReadRepository,
-    @Inject(USER_READ_REPOSITORY_DI_TOKEN) private readonly _userRR: UserReadRepository,
-    @Inject(PASSWORD_SERVICE_DI_TOKEN) private readonly _passwordService: PasswordService,
+    private readonly _adminRR: AdminReadRepository,
+    private readonly _userRR: UserReadRepository,
+    private readonly _passwordService: PasswordService,
   ) {}
 
   async loginAdmin({ username, password }: LoginPayload): Promise<AdminId> {
