49e3173007f3a65c8238b6439937a62d4610c02b
imkzvd <imkzvd@gmail.com>
2025-06-27 16:32:43 +0300

    feat: added "query-bus" lib
    Refs: #107

---
+++ libs/query-bus/src/query-bus.service.ts
@@ -0,0 +1,14 @@
+import { QueryBus as NestQueryBus } from '@nestjs/cqrs';
+import { Injectable } from '@nestjs/common';
+import { Query } from '@types';
+
+type ExtractQueryResponse<TQuery extends Query<any>> = TQuery extends Query<infer R> ? R : never;
+
+@Injectable()
+export class QueryBus {
+  constructor(private readonly _QB: NestQueryBus) {}
+
+  execute<T extends Query<any>>(query: T): Promise<ExtractQueryResponse<T>> {
+    return this._QB.execute(query);
+  }
+}
