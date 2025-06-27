2bdcdc190883e4f71bdea64d947550770a082e2d
imkzvd <imkzvd@gmail.com>
2025-06-28 02:21:36 +0300

    feat: deleted default files in "admin" app
    Refs: #107

--- apps/admin/src/admin.module.ts
+++ apps/admin/src/admin.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
 
 @Module({
   imports: [],
-  controllers: [AdminController],
-  providers: [AdminService],
+  controllers: [],
+  providers: [],
 })
 export class AdminModule {}
