c9788252ccf354193d1f8e156fde3d00adf9a515
imkzvd <imkzvd@gmail.com>
2025-06-29 00:47:30 +0300

    feat: added "swagger" in admin app
    Refs: #107

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Admin API')
+    .setDescription('The API description')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.port ?? 3000);
 }
 
