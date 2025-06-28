54f865ce4af54b4a9440fd651efbe1a4ba17222a
imkzvd <imkzvd@gmail.com>
2025-06-28 22:26:10 +0300

    feat: moved decorators from old admin app to new admin app
    Refs: #107

--- apps/admin/src/decorators/roles.decorator.ts
+++ apps/admin/src/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { AdminRole } from '../../../../core/domain/components/admin/constants/admin-roles';
+import { AdminRole } from '@core/domain/components/admin/constants/admin-roles';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: AdminRole[]) => SetMetadata(ROLES_KEY, roles);
