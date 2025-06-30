36a02d1e97e64df487e65dcec0abd5f0be364a19
imkzvd <imkzvd@gmail.com>
2025-06-30 03:19:23 +0300

    feat: updated "users" module in "admin" app
    Refs: #107

--- apps/admin/src/modules/user/users.module.ts
+++ apps/admin/src/modules/user/users.module.ts
@@ -1,33 +1,62 @@
 import { Module } from '@nestjs/common';
+import { UserService } from '@core/app/components/user/user.service';
+import { EventBus as EventBusPort } from '@core/app/common/ports/event-bus.port';
+import { UserWriteRepository as UserWriteRepositoryPort } from '@core/domain/components/user/repository/user-write-repository.port';
+import { UserReadRepository as UserReadRepositoryPort } from '@core/domain/components/user/repository/user-read-repository.port';
+import { IdService as IdServicePort } from '@core/app/common/ports/id.service.port';
+import { PasswordService as PasswordServicePort } from '@core/app/common/ports/password-service.port';
+import { TmpFileStorage as TmpFileStoragePort } from '@core/app/common/ports/file-storages/tmp-file-storage.port';
+import { UserFileStorage as UserFileStoragePort } from '@core/app/common/ports/file-storages/user-file-storage.port';
+import { UserId } from '@core/domain/components/user/types';
+import { EventBus } from '@infrastructure/event-bus';
+import { UserWriteRepository } from '@infrastructure/mongoose/services/user/user-write-repository.service';
+import { UserReadRepository } from '@infrastructure/mongoose/services/user/user-read-repository.service';
+import { RandomIdModule, RandomIdService } from '@infrastructure/random-id';
+import { PasswordModule, PasswordService } from '@infrastructure/password';
+import { FileStorageModule, TmpFileStorage, UserFileStorage } from '@infrastructure/file-storage';
 import { UsersController } from './users.controller';
-import { PasswordModule } from '../../../../../infrastructure/security/password/password.module';
-import { UserService } from '../../../../../core/app/components/user/user.service';
-import { CreateUserHandler } from '../../../../../core/app/cqrs/user/commands/create-user/create-user.handler';
-import { RegisterUserHandler } from '../../../../../core/app/cqrs/user/commands/register-user/register-user.handler';
-import { UpdateUserHandler } from '../../../../../core/app/cqrs/user/commands/update-user/update-user.handler';
-import { UpdateUserUsernameHandler } from '../../../../../core/app/cqrs/user/commands/update-user-username/update-user-username.handler';
-import { UpdateUserEmailHandler } from '../../../../../core/app/cqrs/user/commands/update-user-email/update-user-email.handler';
-import { UpdateUserPasswordHandler } from '../../../../../core/app/cqrs/user/commands/update-user-password/update-user-password.handler';
-import { RefreshUserPasswordHandler } from '../../../../../core/app/cqrs/user/commands/refresh-user-password/refresh-user-password.handler';
-import { UpdateUserAvatarHandler } from '../../../../../core/app/cqrs/user/commands/update-user-avatar/update-user-avatar.handler';
-import { DeleteUserAvatarHandler } from '../../../../../core/app/cqrs/user/commands/delete-user-avatar/delete-user-avatar.handler';
-import { DeleteUserHandler } from '../../../../../core/app/cqrs/user/commands/delete-user/delete-user.handler';
-import { GetUserHandler } from '../../../../../core/app/cqrs/user/queries/get-user/get-user.handler';
+import { CreateUserHandler } from './commands/create-user.handler';
+import { DeleteUserHandler } from './commands/delete-user.handler';
+import { DeleteUserAvatarHandler } from './commands/delete-user-avatar.handler';
+import { RefreshUserPasswordHandler } from './commands/refresh-user-password.handler';
+import { UpdateUserHandler } from './commands/update-user.handler';
+import { UpdateUserAvatarHandler } from './commands/update-user-avatar.handler';
+import { UpdateUserEmailHandler } from './commands/update-user-email.handler';
+import { UpdateUserUsernameHandler } from './commands/update-user-username.handler';
+import { GetUserHandler } from './queries/get-user.handler';
 
 @Module({
-  imports: [PasswordModule],
+  imports: [PasswordModule, RandomIdModule, FileStorageModule],
   providers: [
-    UserService,
+    {
+      provide: UserService,
+      useFactory: (
+        eb: EventBusPort,
+        wr: UserWriteRepositoryPort,
+        rr: UserReadRepositoryPort,
+        idService: IdServicePort<UserId>,
+        passwordService: PasswordServicePort,
+        tmpFS: TmpFileStoragePort,
+        userFS: UserFileStoragePort,
+      ) => new UserService(eb, wr, rr, idService, passwordService, tmpFS, userFS),
+      inject: [
+        EventBus,
+        UserWriteRepository,
+        UserReadRepository,
+        RandomIdService,
+        PasswordService,
+        TmpFileStorage,
+        UserFileStorage,
+      ],
+    },
     CreateUserHandler,
-    RegisterUserHandler,
-    UpdateUserHandler,
-    UpdateUserUsernameHandler,
-    UpdateUserEmailHandler,
-    UpdateUserPasswordHandler,
-    RefreshUserPasswordHandler,
-    UpdateUserAvatarHandler,
-    DeleteUserAvatarHandler,
     DeleteUserHandler,
+    DeleteUserAvatarHandler,
+    RefreshUserPasswordHandler,
+    UpdateUserHandler,
+    UpdateUserAvatarHandler,
+    UpdateUserEmailHandler,
+    UpdateUserUsernameHandler,
     GetUserHandler,
   ],
   controllers: [UsersController],
