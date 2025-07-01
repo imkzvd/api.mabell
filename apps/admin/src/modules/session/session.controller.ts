3c963d36e184cc62033e70d310b21922ddb1fdc6
imkzvd <imkzvd@gmail.com>
2025-07-01 16:34:26 +0300

    feat: updated admin app "session" module
    Refs: #107

--- apps/admin/src/modules/session/session.controller.ts
+++ apps/admin/src/modules/session/session.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Delete, HttpCode, HttpStatus, Param } from '@nestjs/common';
-import { ApiCookieAuth, ApiNoContentResponse, ApiOperation } from '@nestjs/swagger';
-import { ParseObjectIdPipe } from '../../../common/pipes/parse-object-id.pipe';
-import { CommandBus } from '@nestjs/cqrs';
-import { DeleteAdminRefreshTokenCommand } from '../../../../../core/app/cqrs/token/commands/delete-admin-refresh-token/delete-admin-refresh-token.command';
+import { ApiCookieAuth, ApiNoContentResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CommandBus } from '@infrastructure/command-bus';
+import { AdminRoles } from '@core/domain/components/admin/constants/admin-roles';
+import { ParseObjectIdPipe } from '@shared/pipes/parse-object-id.pipe';
 import { Roles } from '../../decorators/roles.decorator';
-import { AdminRoles } from '../../../../../core/domain/components/admin/constants/admin-roles';
+import { DeleteAdminRefreshTokenByIdCommand } from '@core/app/cqrs/token/commands/delete-admin-refresh-token-by-id/delete-admin-refresh-token-by-id.command';
 
+@ApiTags('Session')
 @ApiCookieAuth()
 @Roles(AdminRoles.Owner, AdminRoles.Admin)
 @Controller('/sessions')
@@ -17,6 +18,6 @@ export class SessionController {
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete('/:id')
   async deleteSession(@Param('id', ParseObjectIdPipe) id: string): Promise<void> {
-    await this._commandBus.execute(new DeleteAdminRefreshTokenCommand(id));
+    await this._commandBus.execute(new DeleteAdminRefreshTokenByIdCommand(id));
   }
 }
--- apps/admin/src/modules/session/session.module.ts
+++ apps/admin/src/modules/session/session.module.ts
@@ -1,12 +1,39 @@
 import { Module } from '@nestjs/common';
 import { SessionController } from './session.controller';
-import { DeleteAdminRefreshTokenHandler } from '../../../../../core/app/cqrs/token/commands/delete-admin-refresh-token/delete-admin-refresh-token.handler';
-import { AdminTokenService } from '../../../../../core/app/components/admin-token/admin-token.service';
-import { JWTModule } from '../../../../../infrastructure/security/jwt/jwt.module';
+import { AdminTokenService } from '@core/app/components/admin-token/admin-token.service';
+import { AdminRefreshTokenWriteRepository as AdminRefreshTokenWriteRepositoryPort } from '@core/domain/components/admin-refresh-token/repository/admin-refresh-token-write-repository.port';
+import { AdminRefreshTokenReadRepository as AdminRefreshTokenReadRepositoryPort } from '@core/domain/components/admin-refresh-token/repository/admin-refresh-token-read-repository.port';
+import { IdService as IdServicePort } from '@core/app/common/ports/id.service.port';
+import { AdminRefreshTokenId } from '@core/domain/components/admin-refresh-token/types';
+import { JWTService as JWTServicePort } from '@core/app/common/ports/jwt.service.port';
+import { AdminRefreshTokenWriteRepository } from '@infrastructure/mongoose/services/admin-refresh-token/admin-refresh-token-write-repository.service';
+import { AdminRefreshTokenReadRepository } from '@infrastructure/mongoose/services/admin-refresh-token/admin-refresh-token-read-repository.service';
+import { RandomIdService } from '@infrastructure/random-id';
+import { JWTModule, JWTService } from '@infrastructure/jwt';
+import { DeleteAdminRefreshTokenByIdHandler } from './commands/delete-admin-refresh-token-by-id.handler';
 
 @Module({
   imports: [JWTModule],
-  providers: [AdminTokenService, DeleteAdminRefreshTokenHandler],
+  providers: [
+    {
+      provide: AdminTokenService,
+      useFactory: (
+        wr: AdminRefreshTokenWriteRepositoryPort,
+        rr: AdminRefreshTokenReadRepositoryPort,
+        idService: IdServicePort<AdminRefreshTokenId>,
+        jwtService: JWTServicePort,
+      ) => {
+        return new AdminTokenService(wr, rr, idService, jwtService);
+      },
+      inject: [
+        AdminRefreshTokenWriteRepository,
+        AdminRefreshTokenReadRepository,
+        RandomIdService,
+        JWTService,
+      ],
+    },
+    DeleteAdminRefreshTokenByIdHandler,
+  ],
   controllers: [SessionController],
 })
 export class SessionModule {}
