cdf2396a7d75ad77e10cf800afaafc0f7b698cc0
imkzvd <imkzvd@gmail.com>
2025-07-02 01:45:18 +0300

    feat: created client "artist" module
    Refs: #107

--- apps/client/src/modules/artist/artist.controller.ts
+++ apps/client/src/modules/artist/artist.controller.ts
@@ -5,24 +5,19 @@ import {
   NotFoundException,
   Param,
   ParseIntPipe,
-  Query,
+  QueryTypes,
 } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { faker } from '@faker-js/faker';
-import { QueryBus } from '@nestjs/cqrs';
 import { ArtistRO } from './ros/artist.ro';
-import { ParseObjectIdPipe } from '../../../common/pipes/parse-object-id.pipe';
-import { AlbumsRO } from '../../../admin/controllers/albums/ros/albums.ro';
-import { TracksRO } from '../../../admin/controllers/tracks/ros/tracks.ro';
-import { GetArtistQuery } from '../../../../../core/app/cqrs/artist/queries/get-artist/get-artist.query';
-import { GetArtistPublicStatusQuery } from '../../../../../core/app/cqrs/artist/queries/get-artist-public-status/get-artist-public-status.query';
-import { GetArtistAlbumsQuery } from '../../../../../core/app/cqrs/album/queries/get-artist-albums/get-artist-albums.query';
-import { GetArtistTracksQuery } from '../../../../../core/app/cqrs/track/queries/get-artist-tracks/get-artist-tracks.query';
+import { ParseObjectIdPipe } from '@shared/pipes/parse-object-id.pipe';
+import { GetArtistQuery } from '@core/app/cqrs/artist/queries/get-artist/get-artist.query';
+import { QueryBus } from '@infrastructure/query-bus';
 
 @ApiTags('Artists')
 @Controller({ path: '/artist' })
-export class ArtistsController {
-  constructor(private readonly _queryBus: QueryBus) {}
+export class ArtistController {
+  constructor(private readonly _QB: QueryBus) {}
 
   @ApiOperation({ summary: 'Get an artist by id', operationId: 'getArtist' })
   @ApiParam({
@@ -34,10 +29,10 @@ export class ArtistsController {
   @ApiOkResponse({ description: 'Artist', type: ArtistRO })
   @Get('/:id')
   async getArtist(@Param('id', ParseObjectIdPipe) id: string): Promise<ArtistRO> {
-    const foundArtist = await this._queryBus.execute(new GetArtistQuery(id, { isPublic: true }));
+    const foundArtist = await this._QB.execute(new GetArtistQuery(id, { isPublic: true }));
 
     if (!foundArtist) {
-      throw new NotFoundException(`There is no artist with the specified ID`);
+      throw new NotFoundException('Artist does not exist');
     }
 
     return new ArtistRO(foundArtist);
@@ -56,16 +51,16 @@ export class ArtistsController {
   @Get('/:id/albums')
   async getArtistAlbums(
     @Param('id', ParseObjectIdPipe) id: string,
-    @Query('limit', new DefaultValuePipe(50), ParseIntPipe) limit: number,
-    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
+    @QueryTypes('limit', new DefaultValuePipe(50), ParseIntPipe) limit: number,
+    @QueryTypes('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
   ): Promise<AlbumsRO> {
-    const isArtistPublic = await this._queryBus.execute(new GetArtistPublicStatusQuery(id));
+    const isArtistPublic = await this._QB.execute(new GetArtistPublicStatusQuery(id));
 
     if (!isArtistPublic) {
       throw new NotFoundException(`There is no artist with the specified ID`);
     }
 
-    const foundAlbums = await this._queryBus.execute(
+    const foundAlbums = await this._QB.execute(
       new GetArtistAlbumsQuery(id, {
         isPublic: true,
         pagination: { limit, offset },
@@ -88,16 +83,16 @@ export class ArtistsController {
   @Get('/:id/tracks')
   async getArtistTracks(
     @Param('id', ParseObjectIdPipe) id: string,
-    @Query('limit', new DefaultValuePipe(50), ParseIntPipe) limit: number,
-    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
+    @QueryTypes('limit', new DefaultValuePipe(50), ParseIntPipe) limit: number,
+    @QueryTypes('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
   ): Promise<TracksRO> {
-    const isArtistPublic = await this._queryBus.execute(new GetArtistPublicStatusQuery(id));
+    const isArtistPublic = await this._QB.execute(new GetArtistPublicStatusQuery(id));
 
     if (!isArtistPublic) {
       throw new NotFoundException(`There is no artist with the specified ID`);
     }
 
-    const foundTracks = await this._queryBus.execute(
+    const foundTracks = await this._QB.execute(
       new GetArtistTracksQuery(id, {
         isPublic: true,
         pagination: { limit, offset },
