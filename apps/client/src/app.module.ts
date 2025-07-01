c4f72ff171fc3405acdbbdcdfa37eeceac45f3c8
imkzvd <imkzvd@gmail.com>
2025-07-02 01:21:33 +0300

    feat: updated client app main module
    Refs: #107

--- apps/client/src/app.module.ts
+++ apps/client/src/app.module.ts
@@ -3,25 +3,23 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { MongooseModule } from '../../../infrastructure/persistence/mongoose/mongoose.module';
 import { UsersModule } from './controllers/users/users.module';
-import { ArtistsModule } from './controllers/artists/artists.module';
-import { IdModule } from '../../../infrastructure/services/id/id.module';
-import { StorageModule } from '../../../infrastructure/storage/storage.module';
-import { InMemoryEventBusModule } from '../../../infrastructure/event-bus/in-memory-event-bus/in-memory-event-bus.module';
-import { LoginModule } from './controllers/login/login.module';
-import appConfig from '../admin/config/app.config';
-import corsConfig from '../admin/config/cors.config';
-import databaseConfig from '../admin/config/database.config';
-import jwtConfig from '../admin/config/jwt.config';
-import mailConfig from '../admin/config/mail.config';
-import redisConfig from '../admin/config/redis.config';
+import { MongooseModule } from '@infrastructure/mongoose';
+import { EventBusModule } from '@infrastructure/event-bus';
+import { CommandBusModule } from '@infrastructure/command-bus';
+import appConfig from './configs/app.config';
+import corsConfig from './configs/cors.config';
+import databaseConfig from './configs/database.config';
+import jwtConfig from './configs/jwt.config';
+import mailConfig from './configs/mail.config';
+import redisConfig from './configs/redis.config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: '.env.client',
       load: [appConfig, corsConfig, databaseConfig, jwtConfig, mailConfig, redisConfig],
     }),
     ServeStaticModule.forRoot({
@@ -34,13 +32,11 @@ import redisConfig from '../admin/config/redis.config';
     }),
     ConfigModule.forRoot(),
     CqrsModule.forRoot(),
-    IdModule,
-    InMemoryEventBusModule,
-    StorageModule,
     MongooseModule,
-    LoginModule,
+    EventBusModule,
+    CommandBusModule,
+    AuthModule,
     UsersModule,
-    ArtistsModule,
   ],
 })
-export class ClientAppModule {}
+export class AppModule {}
