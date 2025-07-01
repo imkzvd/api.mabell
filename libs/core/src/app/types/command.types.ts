127b46a0200b82ade7ab87986c9df80a2f410f09
imkzvd <imkzvd@gmail.com>
2025-07-01 19:16:05 +0300

    fix: fixed export declare
    Refs: #107

--- libs/core/src/app/types/command.types.ts
+++ libs/core/src/app/types/command.types.ts
@@ -1,5 +1,5 @@
 export declare const COMMAND_RESPONSE_TYPE_SYMBOL: unique symbol;
 
-export declare class Command<TResponse> {
+export class Command<TResponse> {
   readonly [COMMAND_RESPONSE_TYPE_SYMBOL]: TResponse;
 }
