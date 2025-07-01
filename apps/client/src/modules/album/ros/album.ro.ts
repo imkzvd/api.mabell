0f24906f0492e7287f37604b3241d5563e346734
imkzvd <imkzvd@gmail.com>
2025-07-02 01:46:41 +0300

    feat: created client "album" module
    Refs: #107

---
+++ apps/client/src/modules/album/ros/album.ro.ts
@@ -0,0 +1,116 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { faker } from '@faker-js/faker';
+import * as process from 'process';
+import { ArtistRO } from '../../artists/ros/artist.ro';
+import { LabelValueRO } from '@shared/ros/label-value.ro';
+import {
+  AlbumTypes,
+  getAlbumTypeLabelByValue,
+} from '@core/domain/components/album/constants/album-types';
+import { Genres, getGenreLabelByValue } from '@core/domain/common/constants/genres';
+import { OffsetLimitPaginationRO } from '@shared/ros/offset-limit-pagination.ro';
+import { TrackRO } from '../../../../../admin/src/modules/track/ros/track.ro';
+
+export class AlbumRO {
+  @ApiProperty({
+    type: String,
+    description: 'Id',
+    example: faker.database.mongodbObjectId(),
+  })
+  id: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Name',
+    example: faker.music.songName(),
+  })
+  name: string;
+
+  @ApiProperty({ type: () => [ArtistRO], description: 'Artists' })
+  artists: ArtistRO[];
+
+  @ApiProperty({
+    type: () => LabelValueRO,
+    description: 'Type',
+    example: new LabelValueRO(AlbumTypes['Album'], getAlbumTypeLabelByValue(AlbumTypes['Album'])),
+  })
+  type: LabelValueRO;
+
+  @ApiProperty({
+    type: () => [LabelValueRO],
+    description: 'Genres',
+    example: new LabelValueRO(Genres['Hip-Hop'], getGenreLabelByValue(Genres['Hip-Hop'])),
+  })
+  genres: LabelValueRO[];
+
+  @ApiProperty({
+    type: String,
+    description: 'Cover',
+    example: faker.image.url(),
+    nullable: true,
+  })
+  cover: string | null;
+
+  @ApiProperty({
+    type: String,
+    description: 'Accent color',
+    example: faker.color.rgb(),
+    nullable: true,
+  })
+  color: string | null;
+
+  @ApiProperty({
+    type: String,
+    description: 'Description',
+    example: faker.lorem.text(),
+  })
+  description: string;
+
+  @ApiProperty({
+    type: Date,
+    description: 'Release date',
+    example: faker.date.past().toISOString(),
+    nullable: true,
+  })
+  releaseAt: Date | null;
+
+  @ApiProperty({ type: () => OffsetLimitPaginationRO<TrackRO>, description: 'Album tracks' })
+  tracks: OffsetLimitPaginationRO<TrackRO>;
+
+  @ApiProperty({ type: Boolean, description: 'Active', example: true })
+  isActive: boolean;
+
+  @ApiProperty({ type: Boolean, description: 'Public', example: true })
+  isPublic: boolean;
+
+  @ApiProperty({
+    type: Date,
+    description: 'Created date',
+    example: faker.date.past().toISOString(),
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    type: Date,
+    description: 'Updated date',
+    example: faker.date.past().toISOString(),
+  })
+  updatedAt: Date;
+
+  constructor(album: AlbumDTO, tracks?: OffsetLimitPaginationResponseDTO<TrackDTO>) {
+    this.id = album.id;
+    this.name = album.name;
+    this.artists = album.artists.map((i) => new ArtistRO(i));
+    this.type = new LabelValueRO(album.type, getAlbumTypeLabelByValue(album.type));
+    this.genres = album.genres.map((genre) => new LabelValueRO(genre, getGenreLabelByValue(genre)));
+    this.cover = album.cover ? `${process.env.HOST}${album.cover}` : null;
+    this.color = album.color;
+    this.description = album.description;
+    this.releaseAt = album.releaseAt;
+    this.tracks = tracks ? new TracksRO(tracks) : new OffsetLimitPaginationRO();
+    this.isActive = album.isActive;
+    this.isPublic = album.isPublic;
+    this.createdAt = album.createdAt;
+    this.updatedAt = album.updatedAt;
+  }
+}
