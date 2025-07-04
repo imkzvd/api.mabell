2e5fcc9a643544f9449c682693dad0523a15142a
imkzvd <imkzvd@gmail.com>
2025-07-04 19:41:59 +0300

    refactor: updated command and query wrapper in admin app "session" module
    Refs: #109

--- apps/admin/src/modules/session/commands/delete-admin-refresh-token-by-id.handler.ts
+++ apps/admin/src/modules/session/commands/delete-admin-refresh-token-by-id.handler.ts
@@ -1,20 +1,12 @@
 import { Inject } from '@nestjs/common';
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler } from '@nestjs/cqrs';
 import { AdminTokenService } from '@core/app/components/admin-token/admin-token.service';
 import { DeleteAdminRefreshTokenByIdCommand } from '@core/app/cqrs/token/commands/delete-admin-refresh-token-by-id/delete-admin-refresh-token-by-id.command';
 import { DeleteAdminRefreshTokenByIdHandler as CoreDeleteAdminRefreshTokenByIdHandler } from '@core/app/cqrs/token/commands/delete-admin-refresh-token-by-id/delete-admin-refresh-token-by-id.handler';
 
 @CommandHandler(DeleteAdminRefreshTokenByIdCommand)
-export class DeleteAdminRefreshTokenByIdHandler
-  implements ICommandHandler<DeleteAdminRefreshTokenByIdCommand>
-{
-  private readonly _coreHandler: CoreDeleteAdminRefreshTokenByIdHandler;
-
+export class DeleteAdminRefreshTokenByIdHandler extends CoreDeleteAdminRefreshTokenByIdHandler {
   constructor(@Inject(AdminTokenService) readonly service: AdminTokenService) {
-    this._coreHandler = new CoreDeleteAdminRefreshTokenByIdHandler(service);
-  }
-
-  execute(command: DeleteAdminRefreshTokenByIdCommand) {
-    return this._coreHandler.execute(command);
+    super(service);
   }
 }
