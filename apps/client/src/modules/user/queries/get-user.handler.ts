7311c714cf43c5de3f1f424953189b12e478ff46
imkzvd <imkzvd@gmail.com>
2025-07-04 20:15:58 +0300

    refactor: updated command and query wrapper in client app "user" module
    Closes: #109

--- apps/client/src/modules/user/queries/get-user.handler.ts
+++ apps/client/src/modules/user/queries/get-user.handler.ts
@@ -1,18 +1,12 @@
-import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { QueryHandler } from '@nestjs/cqrs';
 import { GetUserQuery } from '@core/app/cqrs/user/queries/get-user/get-user.query';
 import { GetUserHandler as CoreGetUserHandler } from '@core/app/cqrs/user/queries/get-user/get-user.handler';
 import { UserService } from '@core/app/components/user/user.service';
 import { Inject } from '@nestjs/common';
 
 @QueryHandler(GetUserQuery)
-export class GetUserHandler implements IQueryHandler<GetUserQuery> {
-  private readonly coreHandler: CoreGetUserHandler;
-
+export class GetUserHandler extends CoreGetUserHandler {
   constructor(@Inject(UserService) readonly userService: UserService) {
-    this.coreHandler = new CoreGetUserHandler(userService);
-  }
-
-  async execute(query: GetUserQuery) {
-    return this.coreHandler.execute(query);
+    super(userService);
   }
 }
