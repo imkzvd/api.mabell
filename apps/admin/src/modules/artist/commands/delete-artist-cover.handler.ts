78770bd7dc1721068236384c55ba7143affb227c
imkzvd <imkzvd@gmail.com>
2025-07-04 17:58:15 +0300

    refactor: updated command and query wrapper in admin app "artist" module
    Refs: #109

--- apps/admin/src/modules/artist/commands/delete-artist-cover.handler.ts
+++ apps/admin/src/modules/artist/commands/delete-artist-cover.handler.ts
@@ -1,18 +1,12 @@
 import { Inject } from '@nestjs/common';
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler } from '@nestjs/cqrs';
 import { ArtistService } from '@core/app/components/artist/artist.service';
 import { DeleteArtistCoverCommand } from '@core/app/cqrs/artist/commands/delete-artist-cover/delete-artist-cover.command';
 import { DeleteArtistCoverHandler as CoreDeleteArtistCoverHandler } from '@core/app/cqrs/artist/commands/delete-artist-cover/delete-artist-cover.handler';
 
 @CommandHandler(DeleteArtistCoverCommand)
-export class DeleteArtistCoverHandler implements ICommandHandler<DeleteArtistCoverCommand> {
-  private readonly _coreHandler: CoreDeleteArtistCoverHandler;
-
+export class DeleteArtistCoverHandler extends CoreDeleteArtistCoverHandler {
   constructor(@Inject(ArtistService) service: ArtistService) {
-    this._coreHandler = new CoreDeleteArtistCoverHandler(service);
-  }
-
-  execute(command: DeleteArtistCoverCommand) {
-    return this._coreHandler.execute(command);
+    super(service);
   }
 }
