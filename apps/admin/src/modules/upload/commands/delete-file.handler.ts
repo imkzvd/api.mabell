8421be32d1f8d71e3be7dcc34d07b2f7f620b827
imkzvd <imkzvd@gmail.com>
2025-07-04 20:03:17 +0300

    refactor: updated command and query wrapper in admin app "upload" module
    Refs: #109

--- apps/admin/src/modules/upload/commands/delete-file.handler.ts
+++ apps/admin/src/modules/upload/commands/delete-file.handler.ts
@@ -1,18 +1,12 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { UploadService } from '@core/app/components/upload/upload.service';
 import { DeleteFileHandler as CoreDeleteFileHandler } from '@core/app/cqrs/upload/commands/delete-file/delete-file.handler';
 import { DeleteFileCommand } from '@core/app/cqrs/upload/commands/delete-file/delete-file.command';
 
 @CommandHandler(DeleteFileCommand)
-export class DeleteFileHandler implements ICommandHandler<DeleteFileCommand> {
-  private readonly _coreHandler: CoreDeleteFileHandler;
-
+export class DeleteFileHandler extends CoreDeleteFileHandler {
   constructor(@Inject(UploadService) service: UploadService) {
-    this._coreHandler = new CoreDeleteFileHandler(service);
-  }
-
-  execute(command: DeleteFileCommand) {
-    return this._coreHandler.execute(command);
+    super(service);
   }
 }
