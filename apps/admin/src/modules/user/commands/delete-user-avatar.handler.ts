af75d83db6a90e6e268da9f0033f760be4e01461
imkzvd <imkzvd@gmail.com>
2025-07-04 20:08:17 +0300

    refactor: updated command and query wrapper in admin app "user" module
    Refs: #109

--- apps/admin/src/modules/user/commands/delete-user-avatar.handler.ts
+++ apps/admin/src/modules/user/commands/delete-user-avatar.handler.ts
@@ -1,18 +1,12 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { UserService } from '@core/app/components/user/user.service';
 import { DeleteUserAvatarHandler as CoreDeleteUserAvatarHandler } from '@core/app/cqrs/user/commands/delete-user-avatar/delete-user-avatar.handler';
 import { DeleteUserAvatarCommand } from '@core/app/cqrs/user/commands/delete-user-avatar/delete-user-avatar.command';
 
 @CommandHandler(DeleteUserAvatarCommand)
-export class DeleteUserAvatarHandler implements ICommandHandler<DeleteUserAvatarCommand> {
-  private readonly _coreHandler: CoreDeleteUserAvatarHandler;
-
+export class DeleteUserAvatarHandler extends CoreDeleteUserAvatarHandler {
   constructor(@Inject(UserService) service: UserService) {
-    this._coreHandler = new CoreDeleteUserAvatarHandler(service);
-  }
-
-  execute(command: DeleteUserAvatarCommand) {
-    return this._coreHandler.execute(command);
+    super(service);
   }
 }
--- apps/admin/src/modules/user/commands/update-user-avatar.handler.ts
+++ apps/admin/src/modules/user/commands/update-user-avatar.handler.ts
@@ -1,18 +1,12 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { UserService } from '@core/app/components/user/user.service';
 import { UpdateUserAvatarCommand } from '@core/app/cqrs/user/commands/update-user-avatar/update-user-avatar.command';
 import { UpdateUserAvatarHandler as CoreUpdateUserAvatarHandler } from '@core/app/cqrs/user/commands/update-user-avatar/update-user-avatar.handler';
 
 @CommandHandler(UpdateUserAvatarCommand)
-export class UpdateUserAvatarHandler implements ICommandHandler<UpdateUserAvatarCommand> {
-  private readonly _coreHandler: CoreUpdateUserAvatarHandler;
-
+export class UpdateUserAvatarHandler extends CoreUpdateUserAvatarHandler {
   constructor(@Inject(UserService) service: UserService) {
-    this._coreHandler = new CoreUpdateUserAvatarHandler(service);
-  }
-
-  execute(command: UpdateUserAvatarCommand) {
-    return this._coreHandler.execute(command);
+    super(service);
   }
 }
