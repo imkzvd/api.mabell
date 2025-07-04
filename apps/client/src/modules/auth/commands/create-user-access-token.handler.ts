5fb1e229d8cd1b62fc46679d691e07913515361b
imkzvd <imkzvd@gmail.com>
2025-07-04 20:13:22 +0300

    refactor: updated command and query wrapper in client app "auth" module
    Refs: #109

--- apps/client/src/modules/auth/commands/create-user-access-token.handler.ts
+++ apps/client/src/modules/auth/commands/create-user-access-token.handler.ts
@@ -1,4 +1,4 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { UserService } from '@core/app/components/user/user.service';
 import { UserTokenService } from '@core/app/components/user-token/user-token.service';
@@ -6,17 +6,11 @@ import { CreateUserAccessTokenCommand } from '@core/app/cqrs/token/commands/crea
 import { CreateUserAccessTokenHandler as CoreCreateUserAccessTokenHandler } from '@core/app/cqrs/token/commands/create-user-access-token/create-user-access-token.handler';
 
 @CommandHandler(CreateUserAccessTokenCommand)
-export class CreateUserAccessTokenHandler implements ICommandHandler<CreateUserAccessTokenCommand> {
-  private readonly _coreHandler: CoreCreateUserAccessTokenHandler;
-
+export class CreateUserAccessTokenHandler extends CoreCreateUserAccessTokenHandler {
   constructor(
     @Inject(UserService) userService: UserService,
     @Inject(UserTokenService) userTokenService: UserTokenService,
   ) {
-    this._coreHandler = new CoreCreateUserAccessTokenHandler(userService, userTokenService);
-  }
-
-  execute(command: CreateUserAccessTokenCommand) {
-    return this._coreHandler.execute(command);
+    super(userService, userTokenService);
   }
 }
