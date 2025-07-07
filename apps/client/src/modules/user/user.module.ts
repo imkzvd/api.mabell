8c5f299a8ffe4585c208860a76b0da858271135a
imkzvd <imkzvd@gmail.com>
2025-07-07 16:54:14 +0300

    fix: added import "FileStorage" module in client app "user" module
    Closes: #111

--- apps/client/src/modules/user/user.module.ts
+++ apps/client/src/modules/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PasswordModule } from '@infrastructure/password';
 import { QueryBusModule } from '@infrastructure/query-bus';
+import { FileStorageModule } from '@infrastructure/file-storage';
 import { GetUserHandler } from './queries/get-user.handler';
 import { UserController } from './user.controller';
 import { userServiceProvider } from '../../providers/user-service.provider';
 
 @Module({
-  imports: [PasswordModule, QueryBusModule],
+  imports: [PasswordModule, QueryBusModule, FileStorageModule],
   providers: [userServiceProvider, GetUserHandler],
   controllers: [UserController],
 })
