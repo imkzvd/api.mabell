867858fc9b86ee813518b462bd6bea18fcc17b4b
imkzvd <imkzvd@gmail.com>
2025-07-07 16:31:01 +0300

    feat: added "upload-service" module in admin app "upload" module
    Refs: #111

--- apps/admin/src/modules/upload/upload.module.ts
+++ apps/admin/src/modules/upload/upload.module.ts
@@ -1,21 +1,16 @@
 import { Module } from '@nestjs/common';
+import { FileStorageModule } from '@infrastructure/file-storage';
 import { UploadController } from './upload.controller';
-import { UploadService } from '@core/app/components/upload/upload.service';
 import { UploadFileHandler } from './commands/upload-file.handler';
 import { DeleteFileHandler } from './commands/delete-file.handler';
 import { DeleteAllFilesHandler } from './commands/delete-all-files.handler';
-import { TmpFileStorage as TmpFileStoragePort } from '@core/app/common/ports/file-storages/tmp-file-storage.port';
-import { FileStorageModule, TmpFileStorage } from '@infrastructure/file-storage';
 import { GetFileHandler } from './queries/get-file.handler';
+import { uploadServiceProvider } from './providers/upload-service.provider';
 
 @Module({
   imports: [FileStorageModule],
   providers: [
-    {
-      provide: UploadService,
-      useFactory: (fs: TmpFileStoragePort) => new UploadService(fs),
-      inject: [TmpFileStorage],
-    },
+    uploadServiceProvider,
     UploadFileHandler,
     DeleteFileHandler,
     DeleteAllFilesHandler,
