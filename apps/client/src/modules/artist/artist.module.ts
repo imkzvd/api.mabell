e7f5dd0141f92eb19fb16d8b85e6b2279df42104
imkzvd <imkzvd@gmail.com>
2025-07-07 16:46:59 +0300

    feat: added "artist-service" provider in client app
    Refs: #111

--- apps/client/src/modules/artist/artist.module.ts
+++ apps/client/src/modules/artist/artist.module.ts
@@ -1,44 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ArtistService } from '@core/app/components/artist/artist.service';
-import { EventBus as EventBusPort } from '@core/app/common/ports/event-bus.port';
-import { ArtistWriteRepository as ArtistWriteRepositoryPort } from '@core/domain/components/artist/repository/artist-write-repository.port';
-import { ArtistReadRepository as ArtistReadRepositoryPort } from '@core/domain/components/artist/repository/artist-read-repository.port';
-import { IdService as IdServicePort } from '@core/app/common/ports/id.service.port';
-import { ArtistId } from '@core/domain/components/artist/types';
-import { TmpFileStorage as TmpFileStoragePort } from '@core/app/common/ports/file-storages/tmp-file-storage.port';
-import { ArtistFileStorage as ArtistFileStoragePort } from '@core/app/common/ports/file-storages/artist-file-storage.port';
-import { EventBus } from '@infrastructure/event-bus';
-import { ArtistWriteRepository } from '@infrastructure/mongoose/services/artist/artist-write-repository.service';
-import { ArtistReadRepository } from '@infrastructure/mongoose/services/artist/artist-read-repository.service';
-import { RandomIdModule, RandomIdService } from '@infrastructure/random-id';
-import { ArtistFileStorage, FileStorageModule, TmpFileStorage } from '@infrastructure/file-storage';
+import { RandomIdModule } from '@infrastructure/random-id';
+import { FileStorageModule } from '@infrastructure/file-storage';
 import { ArtistController } from './artist.controller';
 import { GetArtistHandler } from './queries/get-artist.handler';
+import { artistServiceProvider } from '../../providers/artist-service.provider';
 
 @Module({
   imports: [RandomIdModule, FileStorageModule],
-  providers: [
-    {
-      provide: ArtistService,
-      useFactory: (
-        eb: EventBusPort,
-        wr: ArtistWriteRepositoryPort,
-        rr: ArtistReadRepositoryPort,
-        idService: IdServicePort<ArtistId>,
-        tmpFS: TmpFileStoragePort,
-        artistFS: ArtistFileStoragePort,
-      ) => new ArtistService(eb, wr, rr, idService, tmpFS, artistFS),
-      inject: [
-        EventBus,
-        ArtistWriteRepository,
-        ArtistReadRepository,
-        RandomIdService,
-        TmpFileStorage,
-        ArtistFileStorage,
-      ],
-    },
-    GetArtistHandler,
-  ],
+  providers: [artistServiceProvider, GetArtistHandler],
   controllers: [ArtistController],
 })
 export class ArtistModule {}
