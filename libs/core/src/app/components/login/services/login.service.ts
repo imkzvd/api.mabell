3d27dd2afb64fa488ca4d2c629d12c59d8b57328
imkzvd <imkzvd@gmail.com>
2025-07-09 21:14:43 +0300

    feat: separate core app "login" service
    Refs: #115

--- libs/core/src/app/components/login/services/login.service.ts
+++ libs/core/src/app/components/login/services/login.service.ts
@@ -1,19 +1,16 @@
 import { UnauthorizedException } from '@core/shared/exceptions';
 import { AdminId } from '@core/domain/components/admin/types';
-import { UserId } from '@core/domain/components/user/types';
 import { AdminReadRepository } from '@core/domain/components/admin/repository/admin-read-repository.port';
-import { UserReadRepository } from '@core/domain/components/user/repository/user-read-repository.port';
-import { LoginPayload } from './types';
-import { PasswordService } from '../../common/ports/password-service.port';
+import { LoginPayload } from '../types';
+import { PasswordService } from '../../../common/ports/password-service.port';
 
-export class LoginService {
+export class AdminLoginService {
   constructor(
     private readonly _adminRR: AdminReadRepository,
-    private readonly _userRR: UserReadRepository,
     private readonly _passwordService: PasswordService,
   ) {}
 
-  async loginAdmin({ username, password }: LoginPayload): Promise<AdminId> {
+  async login({ username, password }: LoginPayload): Promise<AdminId> {
     const foundAdmin = await this._adminRR.findByUsername(username);
 
     if (!foundAdmin) {
@@ -31,25 +28,5 @@ export class LoginService {
     }
 
     return foundAdmin.id;
-  }
-
-  async loginUser({ username, password }: LoginPayload): Promise<UserId> {
-    const foundUser = await this._userRR.findByUsername(username);
-
-    if (!foundUser) {
-      throw new UnauthorizedException('Incorrect username or password.');
-    }
-
-    const isValidPassword = await this._passwordService.validate(password, foundUser.password);
-
-    if (!isValidPassword) {
-      throw new UnauthorizedException('Incorrect username or password.');
-    }
-
-    if (foundUser.isBlocked) {
-      throw new UnauthorizedException('Your account is blocked');
-    }
-
-    return foundUser.id;
   }
 }
---
+++ libs/core/src/app/components/login/services/user-login.service.ts
@@ -0,0 +1,32 @@
+import { UnauthorizedException } from '@core/shared/exceptions';
+import { UserId } from '@core/domain/components/user/types';
+import { UserReadRepository } from '@core/domain/components/user/repository/user-read-repository.port';
+import { LoginPayload } from '../types';
+import { PasswordService } from '../../../common/ports/password-service.port';
+
+export class UserLoginService {
+  constructor(
+    private readonly _userRR: UserReadRepository,
+    private readonly _passwordService: PasswordService,
+  ) {}
+
+  async login({ username, password }: LoginPayload): Promise<UserId> {
+    const foundUser = await this._userRR.findByUsername(username);
+
+    if (!foundUser) {
+      throw new UnauthorizedException('Incorrect username or password.');
+    }
+
+    const isValidPassword = await this._passwordService.validate(password, foundUser.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Incorrect username or password.');
+    }
+
+    if (foundUser.isBlocked) {
+      throw new UnauthorizedException('Your account is blocked');
+    }
+
+    return foundUser.id;
+  }
+}
