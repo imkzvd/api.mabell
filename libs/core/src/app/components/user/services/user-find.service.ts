d6c55c21eea5e983b6eb643ab0ba7a74e0015e20
imkzvd <imkzvd@gmail.com>
2025-07-09 20:36:07 +0300

    feat: added core app user verification service
    Refs: #115

--- libs/core/src/app/components/user/services/user-find.service.ts
+++ libs/core/src/app/components/user/services/user-find.service.ts
@@ -1,14 +1,9 @@
-import { UserWriteRepository } from '@core/domain/components/user/repository/user-write-repository.port';
 import { UserReadRepository } from '@core/domain/components/user/repository/user-read-repository.port';
-import { UserId } from '@core/domain/components/user/types';
 import { UserDTO } from '../dtos/user.dto';
 import UserMapper from '../dtos/user.mapper';
 
 export class UserFindService {
-  constructor(
-    private readonly _WR: UserWriteRepository,
-    private readonly _RR: UserReadRepository,
-  ) {}
+  constructor(private readonly _RR: UserReadRepository) {}
 
   async find(id: string, options?: Partial<{ isPublic: boolean }>): Promise<UserDTO | null> {
     const foundUser = await this._RR.findById(id, {
@@ -16,9 +11,5 @@ export class UserFindService {
     });
 
     return foundUser ? UserMapper.toDTO(foundUser) : null;
-  }
-
-  verify(id: string): Promise<UserId | null> {
-    return this._WR.existsById(id);
   }
 }
