ecfacc0888a93cac0a9e199d14df8a85c7ae3554
imkzvd <imkzvd@gmail.com>
2025-07-09 16:40:02 +0300

    feat: separate app "admin" service
    Refs: #115

---
+++ libs/core/src/app/components/admin/services/admin-create.service.ts
@@ -0,0 +1,38 @@
+import { AdminWriteRepository } from '@core/domain/components/admin/repository/admin-write-repository.port';
+import { AdminFactory } from '@core/domain/components/admin/admin.factory';
+import { AdminId } from '@core/domain/components/admin/types';
+import { ADMIN_PASSWORD_LENGTH } from '../constants';
+import { IdService } from '../../../common/ports/id.service.port';
+import { PasswordService } from '../../../common/ports/password-service.port';
+import { EventBus } from '../../../common/ports/event-bus.port';
+import { AdminCreatedEvent } from '../../../common/events/admin-created.event';
+
+export class AdminCreateService {
+  constructor(
+    private readonly _EB: EventBus,
+    private readonly _WR: AdminWriteRepository,
+    private readonly _idService: IdService<AdminId>,
+    private readonly _passwordService: PasswordService,
+  ) {}
+
+  async create(): Promise<AdminId> {
+    const generatedId = this._idService.generate();
+    const { hashPassword } = await this._passwordService.generate({
+      length: ADMIN_PASSWORD_LENGTH,
+      hash: true,
+    });
+
+    const nextAdminIndex = await this._WR.getNextIndex();
+    const createdAdmin = AdminFactory.create({
+      id: generatedId,
+      username: `account${nextAdminIndex}`,
+      password: hashPassword,
+      name: `Account #${nextAdminIndex}`,
+    });
+
+    await this._WR.save(createdAdmin);
+    this._EB.publish(new AdminCreatedEvent({ id: generatedId }));
+
+    return createdAdmin.getId();
+  }
+}
---
+++ libs/core/src/app/components/admin/services/admin-update.service.ts
@@ -0,0 +1,89 @@
+import { DuplicationException, NotFoundException } from '@core/shared/exceptions';
+import { AdminWriteRepository } from '@core/domain/components/admin/repository/admin-write-repository.port';
+import { AdminId } from '@core/domain/components/admin/types';
+import { ADMIN_PASSWORD_LENGTH } from '../constants';
+import { UpdateAdminPayload } from '../types';
+import { PasswordService } from '../../../common/ports/password-service.port';
+import { EventBus } from '../../../common/ports/event-bus.port';
+import { AdminUpdatedEvent } from '../../../common/events/admin-updated.event';
+import { AdminPasswordRefreshedEvent } from '../../../common/events/admin-password-refreshed.event';
+import { AdminBlockedEvent } from '../../../common/events/admin-blocked.event';
+import { AdminUnblockedEvent } from '../../../common/events/admin-unblocked.event';
+
+export class AdminUpdateService {
+  constructor(
+    private readonly _EB: EventBus,
+    private readonly _WR: AdminWriteRepository,
+    private readonly _passwordService: PasswordService,
+  ) {}
+
+  async update(id: string, payload: UpdateAdminPayload): Promise<AdminId> {
+    const foundAdmin = await this._WR.findById(id);
+
+    if (!foundAdmin) {
+      throw new NotFoundException('Admin does not exist');
+    }
+
+    if (payload.name) {
+      foundAdmin.updateName(payload.name);
+    }
+
+    if (payload.role) {
+      foundAdmin.updateRole(payload.role);
+    }
+
+    if (typeof payload.isBlocked === 'boolean') {
+      foundAdmin.updateBlockedStatus(payload.isBlocked);
+
+      this._EB.publish(
+        payload.isBlocked
+          ? new AdminBlockedEvent({ id: foundAdmin.getId() })
+          : new AdminUnblockedEvent({ id: foundAdmin.getId() }),
+      );
+    }
+
+    await this._WR.save(foundAdmin);
+    this._EB.publish(new AdminUpdatedEvent({ id: foundAdmin.getId() }));
+
+    return foundAdmin.getId();
+  }
+
+  async updateUsername(id: string, username: string): Promise<AdminId> {
+    const foundAdmin = await this._WR.findById(id);
+
+    if (!foundAdmin) {
+      throw new NotFoundException(`Admin does not exist`);
+    }
+
+    const existsAdminId = await this._WR.existsByUsername(username);
+
+    if (existsAdminId) {
+      throw new DuplicationException(`The admin with this username already exist`);
+    }
+
+    foundAdmin.updateUsername(username);
+    await this._WR.save(foundAdmin);
+    this._EB.publish(new AdminUpdatedEvent({ id: foundAdmin.getId() }));
+
+    return foundAdmin.getId();
+  }
+
+  async refreshPassword(id: string): Promise<{ id: AdminId; password: string }> {
+    const foundAdmin = await this._WR.findById(id);
+
+    if (!foundAdmin) {
+      throw new NotFoundException('Admin does not exist');
+    }
+
+    const { password, hashPassword } = await this._passwordService.generate({
+      length: ADMIN_PASSWORD_LENGTH,
+      hash: true,
+    });
+    foundAdmin.updatePassword(hashPassword);
+
+    await this._WR.save(foundAdmin);
+    this._EB.publish(new AdminPasswordRefreshedEvent({ id: foundAdmin.getId(), password }));
+
+    return { id: foundAdmin.getId(), password };
+  }
+}
