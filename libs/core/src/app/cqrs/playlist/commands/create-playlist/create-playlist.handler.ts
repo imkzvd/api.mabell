3439a77a1800e95478d4eac49dcae948e6863cb4
imkzvd <imkzvd@gmail.com>
2025-07-09 20:45:22 +0300

    fix: fixed di user verification service in core app "create-playlist" command handler
    Refs: #115

--- libs/core/src/app/cqrs/playlist/commands/create-playlist/create-playlist.handler.ts
+++ libs/core/src/app/cqrs/playlist/commands/create-playlist/create-playlist.handler.ts
@@ -1,17 +1,17 @@
 import { CommandHandler } from '@core/app/types';
 import { NotFoundException } from '@core/shared/exceptions';
 import { PlaylistService } from '@core/app/components/playlist/playlist.service';
-import { UserService } from '@core/app/components/user/user.service';
 import { CreatePlaylistCommand } from '@core/app/cqrs/playlist/commands/create-playlist/create-playlist.command';
+import { UserVerificationService } from '@core/app/components/user/services/user-verification.service';
 
 export class CreatePlaylistHandler implements CommandHandler<CreatePlaylistCommand> {
   constructor(
-    private readonly _userService: UserService,
+    private readonly _userVerificationService: UserVerificationService,
     private readonly _playlistService: PlaylistService,
   ) {}
 
   async execute({ ownerId }: CreatePlaylistCommand) {
-    const verifiedUserId = await this._userService.verifyUserId(ownerId);
+    const verifiedUserId = await this._userVerificationService.verify(ownerId);
 
     if (!verifiedUserId) {
       throw new NotFoundException('User does not exist');
