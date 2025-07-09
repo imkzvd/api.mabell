82a517fe078d025bfffed5e6c72b306b40ff0df7
imkzvd <imkzvd@gmail.com>
2025-07-10 02:18:03 +0300

    feat: separate core app "admin-token" service
    Refs: #115

---
+++ libs/core/src/app/components/admin-token/services/admin-token-delete.service.ts
@@ -0,0 +1,38 @@
+import * as process from 'process';
+import { AdminRefreshTokenId } from '@core/domain/components/admin-refresh-token/types';
+import { NotFoundException } from '@core/shared/exceptions';
+import { AdminRefreshTokenWriteRepository } from '@core/domain/components/admin-refresh-token/repository/admin-refresh-token-write-repository.port';
+import { JWTService } from '../../../common/ports/jwt.service.port';
+import { RefreshTokenPayload } from '../types';
+
+export class AdminTokenDeleteService {
+  constructor(
+    private readonly _WR: AdminRefreshTokenWriteRepository,
+    private readonly _JWTService: JWTService,
+  ) {}
+
+  async deleteRefreshToken(token: string): Promise<void> {
+    const tokenPayload = this._JWTService.decode<RefreshTokenPayload>(
+      token,
+      process.env.REFRESH_TOKEN_SECRET || 'refreshSecret',
+    );
+
+    if (!tokenPayload) return;
+
+    await this._WR.deleteById(tokenPayload.jti);
+  }
+
+  async deleteRefreshTokenById(id: string): Promise<AdminRefreshTokenId> {
+    const deletedRefreshTokenId = await this._WR.deleteById(id);
+
+    if (!deletedRefreshTokenId) {
+      throw new NotFoundException('Refresh token does not exist');
+    }
+
+    return deletedRefreshTokenId;
+  }
+
+  deleteRefreshTokensByAdminId(id: string) {
+    void this._WR.deleteByOwnerId(id);
+  }
+}
