33c52d5a1446e2a2100d84466bdd1e038cf37a43
imkzvd <imkzvd@gmail.com>
2025-07-09 20:27:54 +0300

    feat: updated cqrs app user commands and queries
    Refs: #115

--- libs/core/src/app/cqrs/user/commands/update-user-username/update-user-username.handler.ts
+++ libs/core/src/app/cqrs/user/commands/update-user-username/update-user-username.handler.ts
@@ -1,11 +1,11 @@
 import { CommandHandler } from '@core/app/types';
-import { UserService } from '@core/app/components/user/user.service';
+import { UserUpdateService } from '@core/app/components/user/services/user-update.service';
 import { UpdateUserUsernameCommand } from '@core/app/cqrs/user/commands/update-user-username/update-user-username.command';
 
 export class UpdateUserUsernameHandler implements CommandHandler<UpdateUserUsernameCommand> {
-  constructor(private readonly _userService: UserService) {}
+  constructor(private readonly _service: UserUpdateService) {}
 
   async execute({ id, username }: UpdateUserUsernameCommand) {
-    return await this._userService.updateUserUsername(id, username);
+    return await this._service.updateUsername(id, username);
   }
 }
