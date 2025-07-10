4b693146ccfc41826f96d87de21f6ee6721c5b8c
imkzvd <imkzvd@gmail.com>
2025-07-10 16:59:11 +0300

    feat: added admin token service providers in admin app "admin" module
    Refs: #115

---
+++ apps/admin/src/modules/admin/providers/admin-token-create-service.provider.ts
@@ -0,0 +1,21 @@
+import { Provider } from '@nestjs/common';
+import { AdminTokenCreateService } from '@core/app/components/admin-token/services/admin-token-create.service';
+import { AdminRefreshTokenWriteRepository as AdminRefreshTokenWriteRepositoryPort } from '@core/domain/components/admin-refresh-token/repository/admin-refresh-token-write-repository.port';
+import { IdService as IdServicePort } from '@core/app/common/ports/id.service.port';
+import { AdminRefreshTokenId } from '@core/domain/components/admin-refresh-token/types';
+import { JWTService as JWTServicePort } from '@core/app/common/ports/jwt.service.port';
+import { AdminRefreshTokenWriteRepository } from '@infrastructure/mongoose/services/admin-refresh-token/admin-refresh-token-write-repository.service';
+import { RandomIdService } from '@infrastructure/random-id';
+import { JWTService } from '@infrastructure/jwt';
+
+export const adminTokenCreateServiceProvider: Provider = {
+  provide: AdminTokenCreateService,
+  useFactory: (
+    wr: AdminRefreshTokenWriteRepositoryPort,
+    idService: IdServicePort<AdminRefreshTokenId>,
+    jwtService: JWTServicePort,
+  ) => {
+    return new AdminTokenCreateService(wr, idService, jwtService);
+  },
+  inject: [AdminRefreshTokenWriteRepository, RandomIdService, JWTService],
+};
