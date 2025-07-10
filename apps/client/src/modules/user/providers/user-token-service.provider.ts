c9fbab8c57c6e1cb04800eb477ac80dac702da35
imkzvd <imkzvd@gmail.com>
2025-07-10 05:52:19 +0300

    feat: updated client app "user" module
    Refs: #115

--- apps/client/src/modules/user/providers/user-token-service.provider.ts
+++ apps/client/src/modules/user/providers/user-token-service.provider.ts
@@ -1,29 +1,12 @@
 import { Provider } from '@nestjs/common';
-import { UserTokenService } from '@core/app/components/user-token/user-token.service';
-import { UserRefreshTokenWriteRepository as UserRefreshTokenWriteRepositoryPort } from '@core/domain/components/user-refresh-token/user-refresh-token-write-repository.port';
 import { UserRefreshTokenReadRepository as UserRefreshTokenReadRepositoryPort } from '@core/domain/components/user-refresh-token/user-refresh-token-read-repository.port';
-import { IdService as IdServicePort } from '@core/app/common/ports/id.service.port';
-import { UserRefreshTokenId } from '@core/domain/components/user-refresh-token/types';
-import { JWTService as JWTServicePort } from '@core/app/common/ports/jwt.service.port';
-import { UserRefreshTokenWriteRepository } from '@infrastructure/mongoose/services/user-refresh-token/user-refresh-token-write-repository.service';
 import { UserRefreshTokenReadRepository } from '@infrastructure/mongoose/services/user-refresh-token/user-refresh-token-read-repository.service';
-import { RandomIdService } from '@infrastructure/random-id';
-import { JWTService } from '@infrastructure/jwt';
+import { UserTokenService } from '@core/app/components/user-token/services/user-token.service';
 
 export const userTokenServiceProvider: Provider = {
   provide: UserTokenService,
-  useFactory: (
-    wr: UserRefreshTokenWriteRepositoryPort,
-    rr: UserRefreshTokenReadRepositoryPort,
-    idService: IdServicePort<UserRefreshTokenId>,
-    jwtService: JWTServicePort,
-  ) => {
-    return new UserTokenService(wr, rr, idService, jwtService);
+  useFactory: (rr: UserRefreshTokenReadRepositoryPort) => {
+    return new UserTokenService(rr);
   },
-  inject: [
-    UserRefreshTokenWriteRepository,
-    UserRefreshTokenReadRepository,
-    RandomIdService,
-    JWTService,
-  ],
+  inject: [UserRefreshTokenReadRepository],
 };
