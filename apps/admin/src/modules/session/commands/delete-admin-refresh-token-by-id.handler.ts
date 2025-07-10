82535d5099ada0062c0a8bcd79802b1ba098bb19
imkzvd <imkzvd@gmail.com>
2025-07-10 05:29:04 +0300

    feat: updated admin app "session" module
    Refs: #115

--- apps/admin/src/modules/session/commands/delete-admin-refresh-token-by-id.handler.ts
+++ apps/admin/src/modules/session/commands/delete-admin-refresh-token-by-id.handler.ts
@@ -1,12 +1,11 @@
-import { Inject } from '@nestjs/common';
 import { CommandHandler } from '@nestjs/cqrs';
-import { AdminTokenService } from '@core/app/components/admin-token/admin-token.service';
 import { DeleteAdminRefreshTokenByIdCommand } from '@core/app/cqrs/token/commands/delete-admin-refresh-token-by-id/delete-admin-refresh-token-by-id.command';
 import { DeleteAdminRefreshTokenByIdHandler as CoreDeleteAdminRefreshTokenByIdHandler } from '@core/app/cqrs/token/commands/delete-admin-refresh-token-by-id/delete-admin-refresh-token-by-id.handler';
+import { AdminTokenDeleteService } from '@core/app/components/admin-token/services/admin-token-delete.service';
 
 @CommandHandler(DeleteAdminRefreshTokenByIdCommand)
 export class DeleteAdminRefreshTokenByIdHandler extends CoreDeleteAdminRefreshTokenByIdHandler {
-  constructor(@Inject(AdminTokenService) readonly service: AdminTokenService) {
+  constructor(readonly service: AdminTokenDeleteService) {
     super(service);
   }
 }
--- apps/admin/src/modules/session/session.module.ts
+++ apps/admin/src/modules/session/session.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JWTModule } from '@infrastructure/jwt';
 import { DeleteAdminRefreshTokenByIdHandler } from './commands/delete-admin-refresh-token-by-id.handler';
-import { adminTokenServiceProvider } from '../../providers/admin-token-service.provider';
 import { SessionController } from './session.controller';
+import { adminTokenServiceProvider } from '../admin/providers/admin-token-service.provider';
 
 @Module({
   imports: [JWTModule],
