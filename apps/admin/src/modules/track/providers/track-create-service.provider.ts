4777d708b23a2ca44769c31e7594e7302d3a93f9
imkzvd <imkzvd@gmail.com>
2025-07-11 05:14:16 +0300

    feat: injected track rr in track service providers in admin app
    Refs: #113

--- apps/admin/src/modules/track/providers/track-create-service.provider.ts
+++ apps/admin/src/modules/track/providers/track-create-service.provider.ts
@@ -2,15 +2,21 @@ import { Provider } from '@nestjs/common';
 import { TrackCreateService } from '@core/app/components/track/services/track-create.service';
 import { EventBus as EventBusPort } from '@core/app/common/ports/event-bus.port';
 import { TrackWriteRepository as TrackWriteRepositoryPort } from '@core/domain/components/track/repository/track-write-repository.port';
+import { TrackReadRepository as TrackReadRepositoryPort } from '@core/domain/components/track/repository/track-read-repository.port';
 import { IdService as IdServicePort } from '@core/app/common/ports/id.service.port';
 import { TrackId } from '@core/domain/components/track/types';
 import { EventBus } from '@infrastructure/event-bus';
 import { TrackWriteRepository } from '@infrastructure/mongoose/services/track/track-write-repository.service';
 import { RandomIdService } from '@infrastructure/random-id';
+import { TrackReadRepository } from '@infrastructure/mongoose/services/track/track-read-repository.service';
 
 export const trackCreateServiceProvider: Provider = {
   provide: TrackCreateService,
-  useFactory: (eb: EventBusPort, wr: TrackWriteRepositoryPort, idService: IdServicePort<TrackId>) =>
-    new TrackCreateService(eb, wr, idService),
-  inject: [EventBus, TrackWriteRepository, RandomIdService],
+  useFactory: (
+    eb: EventBusPort,
+    wr: TrackWriteRepositoryPort,
+    rr: TrackReadRepositoryPort,
+    idService: IdServicePort<TrackId>,
+  ) => new TrackCreateService(eb, wr, rr, idService),
+  inject: [EventBus, TrackWriteRepository, TrackReadRepository, RandomIdService],
 };
--- apps/admin/src/modules/track/providers/track-update-service.provider.ts
+++ apps/admin/src/modules/track/providers/track-update-service.provider.ts
@@ -1,20 +1,23 @@
 import { Provider } from '@nestjs/common';
 import { EventBus as EventBusPort } from '@core/app/common/ports/event-bus.port';
 import { TrackWriteRepository as TrackWriteRepositoryPort } from '@core/domain/components/track/repository/track-write-repository.port';
+import { TrackReadRepository as TrackReadRepositoryPort } from '@core/domain/components/track/repository/track-read-repository.port';
 import { EventBus } from '@infrastructure/event-bus';
 import { TrackWriteRepository } from '@infrastructure/mongoose/services/track/track-write-repository.service';
 import { ArtistFileStorage as ArtistFileStoragePort } from '@core/app/common/ports/file-storages/artist-file-storage.port';
 import { ArtistFileStorage, TmpFileStorage } from '@infrastructure/file-storage';
 import { TrackUpdateService } from '@core/app/components/track/services/track-update.service';
 import { TmpFileStorage as TmpFileStoragePort } from '@core/app/common/ports/file-storages/tmp-file-storage.port';
+import { TrackReadRepository } from '@infrastructure/mongoose/services/track/track-read-repository.service';
 
 export const trackUpdateServiceProvider: Provider = {
   provide: TrackUpdateService,
   useFactory: (
     eb: EventBusPort,
     wr: TrackWriteRepositoryPort,
+    rr: TrackReadRepositoryPort,
     tmpFS: TmpFileStoragePort,
     artistFS: ArtistFileStoragePort,
-  ) => new TrackUpdateService(eb, wr, tmpFS, artistFS),
-  inject: [EventBus, TrackWriteRepository, TmpFileStorage, ArtistFileStorage],
+  ) => new TrackUpdateService(eb, wr, rr, tmpFS, artistFS),
+  inject: [EventBus, TrackWriteRepository, TrackReadRepository, TmpFileStorage, ArtistFileStorage],
 };
