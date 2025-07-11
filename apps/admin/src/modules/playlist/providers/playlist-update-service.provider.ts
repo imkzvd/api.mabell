a256a6e2f142f8ecfba53bea57b4cdf25deed582
imkzvd <imkzvd@gmail.com>
2025-07-11 05:23:31 +0300

    feat: injected playlist rr in playlist service providers in admin app
    Refs: #113

--- apps/admin/src/modules/playlist/providers/playlist-update-service.provider.ts
+++ apps/admin/src/modules/playlist/providers/playlist-update-service.provider.ts
@@ -2,19 +2,28 @@ import { Provider } from '@nestjs/common';
 import { PlaylistUpdateService } from '@core/app/components/playlist/services/playlist-update.service';
 import { EventBus as EventBusPort } from '@core/app/common/ports/event-bus.port';
 import { PlaylistWriteRepository as PlaylistWriteRepositoryPort } from '@core/domain/components/playlist/repository/playlist-write-repository.port';
+import { PlaylistReadRepository as PlaylistReadRepositoryPort } from '@core/domain/components/playlist/repository/playlist-read-repository.port';
 import { TmpFileStorage as TmpFileStoragePort } from '@core/app/common/ports/file-storages/tmp-file-storage.port';
 import { UserFileStorage as UserFileStoragePort } from '@core/app/common/ports/file-storages/user-file-storage.port';
 import { EventBus } from '@infrastructure/event-bus';
 import { PlaylistWriteRepository } from '@infrastructure/mongoose/services/playlist/playlist-write-repository.service';
 import { TmpFileStorage, UserFileStorage } from '@infrastructure/file-storage';
+import { PlaylistReadRepository } from '@infrastructure/mongoose/services/playlist/playlist-read-repository.service';
 
 export const playlistUpdateServiceProvider: Provider = {
   provide: PlaylistUpdateService,
   useFactory: (
     eb: EventBusPort,
     wr: PlaylistWriteRepositoryPort,
+    rr: PlaylistReadRepositoryPort,
     tmpFS: TmpFileStoragePort,
     userFS: UserFileStoragePort,
-  ) => new PlaylistUpdateService(eb, wr, tmpFS, userFS),
-  inject: [EventBus, PlaylistWriteRepository, TmpFileStorage, UserFileStorage],
+  ) => new PlaylistUpdateService(eb, wr, rr, tmpFS, userFS),
+  inject: [
+    EventBus,
+    PlaylistWriteRepository,
+    PlaylistReadRepository,
+    TmpFileStorage,
+    UserFileStorage,
+  ],
 };
