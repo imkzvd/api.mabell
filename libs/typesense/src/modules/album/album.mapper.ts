0eb3560ae2fbef86d171afb0d508ab0f70ccbb66
imkzvd <imkzvd@gmail.com>
2025-07-13 12:19:49 +0300

    fix: fixed type in album module mapper in "typesense" lib
    Refs: #117

--- libs/typesense/src/modules/album/album.mapper.ts
+++ libs/typesense/src/modules/album/album.mapper.ts
@@ -1,14 +1,13 @@
 import { IndexedAlbumDTO } from '@core/app/common/ports/search-service/dtos/indexed-album.dto';
 import { IndexedSimplifiedArtistDTO } from '@core/app/common/ports/search-service/dtos/indexed-simplified-artist.dto';
-import { AlbumDTO } from '@core/app/components/album/dtos/album.dto';
 import { BaseMapper } from '@infrastructure/typesense/base/base-mapper.interface';
 import { AlbumPayload } from '@infrastructure/typesense/modules/album/types';
 import { Album } from '@infrastructure/typesense/modules/album/album.document';
 import { AlbumFactory } from '@infrastructure/typesense/modules/album/album.factory';
 
 class AlbumMapper implements BaseMapper<Album, IndexedAlbumDTO, AlbumPayload> {
-  toDocument(dto: AlbumDTO): Album {
-    return AlbumFactory.create(dto);
+  toDocument(payload: AlbumPayload): Album {
+    return AlbumFactory.create(payload);
   }
 
   toDTO(doc: Album): IndexedAlbumDTO {
