0a7ba9d0216a56ddd696f352bfe6330c82950fec
imkzvd <imkzvd@gmail.com>
2025-07-13 08:53:17 +0300

    feat: updated user module in "typesense" lib
    Refs: #117

--- libs/typesense/src/modules/user/event-handlers/create-user-document-on-user-created.event-handler.ts
+++ libs/typesense/src/modules/user/event-handlers/create-user-document-on-user-created.event-handler.ts
@@ -1,11 +1,11 @@
 import { Inject } from '@nestjs/common';
 import { EventHandler } from '@core/app/common/ports/event-bus.port';
-import { UserCollection } from '@infrastructure/typesense/modules/user/user.collection';
 import { UserUpdatedEvent } from '@core/app/common/events/user/user-updated.event';
 import { UserCreatedEvent } from '@core/app/common/events/user/user-created.event';
 import { UserRegisteredEvent } from '@core/app/common/events/user/user-registered.event';
+import { UserCollection } from '@infrastructure/typesense/modules/user/user.collection';
 
-export class SaveUserDocumentOnUserUpdatedEventHandler extends EventHandler<
+export class CreateUserDocumentOnUserCreatedEventHandler extends EventHandler<
   UserCreatedEvent | UserRegisteredEvent | UserUpdatedEvent
 > {
   constructor(@Inject(UserCollection) private readonly _collection: UserCollection) {
--- libs/typesense/src/modules/user/user.document.ts
+++ libs/typesense/src/modules/user/user.document.ts
@@ -1,8 +1,8 @@
 export class User {
   constructor(
-    public readonly id: string,
-    public readonly name: string,
-    public readonly email?: string,
-    public readonly avatar?: string,
+    public id: string,
+    public name: string,
+    public email?: string,
+    public avatar?: string,
   ) {}
 }
