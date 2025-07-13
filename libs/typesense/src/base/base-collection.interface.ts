69d5ba083e35d0de741fe139cf8619a0b3a0dad7
imkzvd <imkzvd@gmail.com>
2025-07-13 08:57:18 +0300

    feat: deleted base collection interface in "typesense" lib
    Refs: #117

--- libs/typesense/src/base/base-collection.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface BaseCollection<Payload, IndexedDTO> {
-  save(payload: Payload): Promise<void>;
-
-  search(q: string): Promise<IndexedDTO[]>;
-
-  delete(id: string): Promise<void>;
-}
