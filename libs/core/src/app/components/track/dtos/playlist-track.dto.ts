b8c9e5216da4c08ee6eddea94f80a52fbc7ef79a
imkzvd <imkzvd@gmail.com>
2025-08-06 04:49:24 +0300

    feat: updated ros and dtos
    Refs: #124

--- libs/core/src/app/components/track/dtos/playlist-track.dto.ts
+++ libs/core/src/app/components/track/dtos/playlist-track.dto.ts
@@ -1,8 +1,10 @@
 import { TrackDTO } from './track.dto';
+import { TrackId } from '@core/domain/components/track/types';
 
 export class PlaylistTrackDTO {
   constructor(
     public readonly track: TrackDTO | null,
+    public readonly trackId: TrackId,
     public readonly addedAt: Date,
   ) {}
 }
