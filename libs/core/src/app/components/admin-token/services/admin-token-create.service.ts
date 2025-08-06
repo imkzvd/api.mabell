f392e167cd95b35b7857af335b87f612ddfc7375
imkzvd <imkzvd@gmail.com>
2025-08-06 04:57:40 +0300

    feat: replaced process env to config service in admin token service
    Refs: #124

--- libs/core/src/app/components/admin-token/services/admin-token-create.service.ts
+++ libs/core/src/app/components/admin-token/services/admin-token-create.service.ts
@@ -1,4 +1,4 @@
-import * as process from 'process';
+import { ConfigService } from '@nestjs/config';
 import { AdminRefreshTokenFactory } from '@core/domain/components/admin-refresh-token/admin-refresh-token.factory';
 import { AdminRefreshTokenId } from '@core/domain/components/admin-refresh-token/types';
 import { AdminRefreshTokenWriteRepository } from '@core/domain/components/admin-refresh-token/repository/admin-refresh-token-write-repository.port';
@@ -15,17 +15,19 @@ export class AdminTokenCreateService {
     private readonly _WR: AdminRefreshTokenWriteRepository,
     private readonly _IdService: IdService<AdminRefreshTokenId>,
     private readonly _JWTService: JWTService,
+    private readonly _configService: ConfigService,
   ) {}
 
   createAccessToken(payload: CreateAccessTokenPayload): string {
+    const accessTokenSecret = this._configService.get<string>('jwt.accessToken.secret');
+    const accessTokenExpiration = this._configService.get<number>('jwt.accessToken.expiresIn');
+
     return this._JWTService.create<AccessTokenCustomPayload>({
       subject: payload.adminId,
       type: TokenTypes.Access,
       payload: { role: payload.role },
-      secret: process.env.ACCESS_TOKEN_SECRET || 'accessSecret',
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRATION
-        ? parseInt(process.env.ACCESS_TOKEN_EXPIRATION)
-        : 300,
+      secret: accessTokenSecret || 'accessSecret',
+      expiresIn: accessTokenExpiration || 300,
     });
   }
 
@@ -46,14 +48,15 @@ export class AdminTokenCreateService {
 
     await this._WR.save(createdRefreshedToken);
 
+    const refreshTokenSecret = this._configService.get<string>('jwt.refreshToken.secret');
+    const refreshTokenExpiration = this._configService.get<number>('jwt.refreshToken.expiresIn');
+
     return this._JWTService.create({
       jti: createdRefreshedToken.getId(),
       subject: createdRefreshedToken.getOwner(),
       type: TokenTypes.Refresh,
-      secret: process.env.REFRESH_TOKEN_SECRET || 'refreshSecret',
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRATION
-        ? parseInt(process.env.REFRESH_TOKEN_EXPIRATION)
-        : 600,
+      secret: refreshTokenSecret || 'refreshSecret',
+      expiresIn: refreshTokenExpiration || 300,
     });
   }
 }
