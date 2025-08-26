ad3f1c08fb7efda45279de3d21011c5b6adf3c2e
imkzvd <imkzvd@gmail.com>
2025-08-26 23:00:35 +0300

    fix: added "secure" prop
    Closes: #164

--- apps/client/src/modules/auth/auth.controller.ts
+++ apps/client/src/modules/auth/auth.controller.ts
@@ -47,11 +47,13 @@ export class AuthController {
 
     res.cookie(ACCESS_TOKEN_COOKIE_NAME, accessToken, {
       httpOnly: true,
+      secure: true,
       maxAge: (accessTokenExpiresIn ? accessTokenExpiresIn : 300) * 1000,
     });
 
     res.cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken, {
       httpOnly: true,
+      secure: true,
       maxAge: (refreshTokenExpiresIn ? refreshTokenExpiresIn : 600) * 1000,
     });
   }
