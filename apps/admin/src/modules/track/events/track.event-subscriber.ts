88998ccf6c73e25b59f65450be2223878c19d8f3
imkzvd <imkzvd@gmail.com>
2025-09-20 13:20:22 +0300

    feat: updated track event subscriber for admin app
    Refs: #185

--- apps/admin/src/modules/track/events/track.event-subscriber.ts
+++ apps/admin/src/modules/track/events/track.event-subscriber.ts
@@ -5,14 +5,22 @@ import { App } from '@api.mabell/core';
 export class TrackEventSubscriber {
   constructor(
     @Inject(EventBus) private readonly _EB: EventBus,
+    @Inject(App.Components.Track.TrackUpdateService)
+    private readonly _updateService: App.Components.Track.TrackUpdateService,
     @Inject(App.Components.Track.TrackDeleteService)
-    private readonly _service: App.Components.Track.TrackDeleteService,
+    private readonly _deleteService: App.Components.Track.TrackDeleteService,
   ) {
+    const updateTracksOnAlbumArtistsUpdatedEventHandler =
+      new App.Components.Track.UpdateTracksOnAlbumArtistsUpdatedEventHandler(this._updateService);
     const deleteTracksOnArtistDeletedEventHandler =
-      new App.Components.Track.DeleteTracksOnArtistDeletedEventHandler(this._service);
+      new App.Components.Track.DeleteTracksOnArtistDeletedEventHandler(this._deleteService);
     const deleteTracksOnAlbumDeletedEventHandler =
-      new App.Components.Track.DeleteTracksOnAlbumDeletedEventHandler(this._service);
+      new App.Components.Track.DeleteTracksOnAlbumDeletedEventHandler(this._deleteService);
 
+    this._EB.subscribe(
+      App.Events.AlbumArtistsUpdatedEvent,
+      updateTracksOnAlbumArtistsUpdatedEventHandler,
+    );
     this._EB.subscribe(App.Events.ArtistDeletedEvent, deleteTracksOnArtistDeletedEventHandler);
     this._EB.subscribe(App.Events.AlbumDeletedEvent, deleteTracksOnAlbumDeletedEventHandler);
   }
