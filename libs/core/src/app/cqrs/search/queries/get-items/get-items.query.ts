68da4034512775a06ae1000d98666a5a08c02e2f
imkzvd <imkzvd@gmail.com>
2025-10-01 17:06:59 +0300

    feat: updated search app "get-items" cqrs query
    Refs: #191

--- libs/core/src/app/cqrs/search/queries/get-items/get-items.query.ts
+++ libs/core/src/app/cqrs/search/queries/get-items/get-items.query.ts
@@ -1,8 +1,12 @@
 import { Query } from '../../../../types';
 import { IndexedItemsDTO } from '../../../../dtos';
+import { SEARCH_COLLECTIONS } from '../../../../ports';
 
 export class GetItemsQuery extends Query<IndexedItemsDTO> {
-  constructor(public readonly q: string) {
+  constructor(
+    public readonly q: string,
+    public readonly options?: Partial<{ collections: SEARCH_COLLECTIONS[]; isGlobal: boolean }>,
+  ) {
     super();
   }
 }
