4494b5d4ba4c25c542846c049e5fb296c04cecaa
imkzvd <imkzvd@gmail.com>
2025-10-01 17:14:26 +0300

    feat: updated client app config
    Refs: #191

--- apps/client/src/configs/app.config.ts
+++ apps/client/src/configs/app.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.APP_PORT || '4000', 10),
-  apiURL: process.env.API_URL,
+  port: parseInt(process.env.CLIENT_APP_PORT || '4000', 10),
+  apiURL: process.env.CLIENT_API_URL,
   env: process.env.NODE_ENV,
 }));
