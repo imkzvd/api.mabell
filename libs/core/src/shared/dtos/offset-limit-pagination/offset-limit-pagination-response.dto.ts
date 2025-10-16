59aff9ea7c1be0c3d49a150ee7fd76e7c7b38524
imkzvd <imkzvd@gmail.com>
2025-10-16 03:52:10 +0300

    refactor: updated offset-limit-pagination-response dto
    Refs: #201

--- libs/core/src/shared/dtos/offset-limit-pagination/offset-limit-pagination-response.dto.ts
+++ libs/core/src/shared/dtos/offset-limit-pagination/offset-limit-pagination-response.dto.ts
@@ -1,9 +1,12 @@
 export class OffsetLimitPaginationResponseDTO<Item extends Record<string, any> | string> {
+  public readonly hasMore: boolean;
+
   constructor(
-    public readonly items: Item[],
-    public readonly total: number,
-    public readonly offset: number,
-    public readonly limit: number,
-    public readonly hasMore: boolean,
-  ) {}
+    public readonly items: Item[] = [],
+    public readonly total: number = 0,
+    public readonly offset: number = 0,
+    public readonly limit: number = 50,
+  ) {
+    this.hasMore = this.total > this.limit + this.offset;
+  }
 }
